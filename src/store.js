4542da96e2851866db7308b4dc628bc29e216335
sirbeefalot <sirbeefalot@pm.me>
2021-07-10 16:17:52 -0300

    remove redux-devtools compose

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
-import {applyMiddleware, createStore, compose} from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./features/redux/reducers";
 import thunk from "redux-thunk";
 
 const store = createStore(
     rootReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(thunk),
 );
+
 export default store;
