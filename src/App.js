25d6adbbed03541fc6ae432e684d15fc1f05a413
superbeefyboy <superbeefyboy@pm.me>
2021-08-06 14:11:57 -0300

    hash routing for ipfs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import { createTheme } from '@material-ui/core/styles';
