9d6e5dc2ca261e6980cc19d4f3e550c9d6d2065b
erenjaegar77 <eerenjaegar77@protonmail.com>
2021-08-11 15:59:55 -0300

    fix bullky tabs

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -1,29 +1,28 @@
 const styles = theme => ({
-    container: {
+  container: {},
+  tabs: {
+    backgroundColor: '#14182B',
+    borderRadius: 40,
+    '& .MuiTabs-indicator': {
+      display: 'none',
+      color: 'transparent',
     },
-    tabs: {
-        backgroundColor: '#14182B',
-        borderRadius: 20,
-        height: 38,
-        '& .MuiTabs-indicator': {
-            display: 'none',
-            color: 'transparent',
-        },
-        '& .MuiTab-root': {
-            minWidth: 70,
-        },
-        '& .MuiTab-textColorPrimary': {
-            fontWeight: 600,
-            letterSpacing: 0.2,
-            color: '#484F7F',
-        },
-        '& .Mui-selected': {
-            backgroundColor: '#484F7F',
-            borderRadius: 20,
-            color: 'white',
-            border: '3px solid #14182B',
-        },
+    '& .MuiTab-root': {
+      minWidth: 70,
     },
+    '& .MuiTab-textColorPrimary': {
+      fontWeight: 600,
+      letterSpacing: 0.2,
+      color: '#484F7F',
+    },
+    '& .Mui-selected': {
+      backgroundColor: '#484F7F',
+      borderRadius: 40,
+      color: 'white',
+      border: '3px solid #14182B',
+      padding: '5px',
+    },
+  },
 });
 
 export default styles;
