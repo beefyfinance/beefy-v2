e5b289d11147bcace938c37e19d69e558f1bbd6d
sirbeefalot <sirbeefalot@pm.me>
2021-08-21 14:27:45 -0300

    remove compose

--- src/store.js
+++ src/store.js
@@ -1,13 +1,7 @@
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './features/redux/reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
