53b5d635584124742f4c7fc4ac0432c46c89284c
erenjaegar77 <eerenjaegar77@protonmail.com>
2021-11-05 11:52:26 -0300

    disable useIsBoosted until know boost and gov vaults

--- src/features/home/hooks/useIsBoosted.tsx
+++ src/features/home/hooks/useIsBoosted.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import lodash from 'lodash';
 import { useDispatch } from 'react-redux';
@@ -8,17 +9,17 @@ export const useIsBoosted = item => {
   const [state, setState] = React.useState({ isBoosted: false, data: null });
   const dispatch = useDispatch();
   const data = useVaults();
-  const boostVaults = data[6];
-  React.useEffect(() => {
-    const boostedVault = lodash.filter(boostVaults, { poolId: item.id, status: 'active' });
+  // const boostVaults = data[6];
+  // // React.useEffect(() => {
+  // //   const boostedVault = lodash.filter(boostVaults, { poolId: item.id, status: 'active' });
 
-    if (boostedVault.length !== 0) {
-      dispatch(reduxActions.vault.fetchBoosts(boostedVault[0]));
-      setState({ isBoosted: true, data: boostedVault[0] });
-      item.tags.push('boost');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // //   if (boostedVault.length !== 0) {
+  // //     dispatch(reduxActions.vault.fetchBoosts(boostedVault[0]));
+  // //     setState({ isBoosted: true, data: boostedVault[0] });
+  // //     item.tags.push('boost');
+  // //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return state;
 };
