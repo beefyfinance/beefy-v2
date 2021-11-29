5a7c8ee2a317a8ca58955b3bcfad7cd696b2ce1a
erenjaegar77 <eerenjaegar77@protonmail.com>
2021-11-29 13:23:17 -0300

    update zaps

--- src/config/zap/cronos.tsx
+++ src/config/zap/cronos.tsx
@@ -6,4 +6,11 @@ export const zaps = [
     ammFactory: '0x3B44B2a187a7b3824131F8db5a74194D0a42Fc15',
     ammPairInitHash: '0xa77ee1cc0f39570ddde947459e293d7ebc2c30ff4e8fc45860afdcb2c2d3dc17',
   },
+  {
+    // CronaSwap
+    zapAddress: '0xb25eB9105549627050AAB3A1c909fBD454014beA',
+    ammRouter: '0xcd7d16fB918511BF7269eC4f48d61D79Fb26f918',
+    ammFactory: '0x73A48f8f521EB31c55c0e1274dB0898dE599Cb11',
+    ammPairInitHash: '0xc93158cffa5b575e32566e81e847754ce517f8fa988d3e25cf346d916216e06f',
+  },
 ];
--- src/config/zap/moonriver.tsx
+++ src/config/zap/moonriver.tsx
@@ -1,1 +1,9 @@
-export const zaps = [];
+export const zaps = [
+  {
+    // Solarbeam
+    zapAddress: '0xb0F9c6FBcfE226EEAD3AE8b019ce4666cE223a78',
+    ammRouter: '0xAA30eF758139ae4a7f798112902Bf6d65612045f',
+    ammFactory: '0x049581aEB6Fe262727f290165C29BDAB065a1B68',
+    ammPairInitHash: '0x9a100ded5f254443fbd264cb7e87831e398a8b642e061670a9bc35ba27293dbf',
+  },
+];
