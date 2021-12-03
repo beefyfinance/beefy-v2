2bac2bff91d058df911bad0f3eb368c7f4274bec
erenjaegar77 <eerenjaegar77@protonmail.com>
2021-12-02 21:40:15 -0300

    fix $NaN balance

--- src/features/home/components/Portfolio/Stats/Stats.tsx
+++ src/features/home/components/Portfolio/Stats/Stats.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Box, makeStyles, Typography, Grid } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import BigNumber from 'bignumber.js';
-
 import { formatUsd } from '../../../../../helpers/format';
 import { styles } from './styles';
 
@@ -20,7 +19,7 @@ export const Stats = ({ stats, blurred }) => {
     <span className={blurred ? classes.blurred : ''}>{blurred ? '$100' : value}</span>
   );
 
-  const formatStat = value => (empty ? '0' : formatUsd(value));
+  const formatStat = value => (empty ? formatUsd(0) : formatUsd(value));
 
   return (
     <Grid container className={classes.stats}>
