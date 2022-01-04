026a9751f37860f2e1320d7323606578103a57de
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-01-04 20:28:26 -0300

    removed not used import

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-globals */
-import React, { memo, useMemo, useEffect } from 'react';
+import React, { memo, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Container, makeStyles, useMediaQuery } from '@material-ui/core';
