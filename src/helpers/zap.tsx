8534732dbbb2f5953e8e597afb43820fd26601de
Wivern <echo@wivern.co.uk>
2022-01-04 22:13:20 +0100

    Update blockchain-addressbook

--- src/helpers/zap.tsx
+++ src/helpers/zap.tsx
@@ -3,15 +3,14 @@ import { getCreate2Address } from '@ethersproject/address';
 import { addressBook as _addressBook } from 'blockchain-addressbook';
 import { config } from '../config/config';
 
-//allow the Harmony-blockchain entries in the address-book to be accessed via the normal  
+//allow the Harmony-blockchain entries in the address-book to be accessed via the normal
 //  "network" property values used in our core vault-object schema
-const addressBook = {..._addressBook, harmony: _addressBook.one};
+const addressBook = { ..._addressBook, harmony: _addressBook.one };
 
 const data = [];
 for (let net in config) {
   data[net] = require(`../config/zap/${net}`);
 }
-
 
 export const getEligibleZap = pool => {
   if (pool.assets.length !== 2) return undefined;
@@ -50,7 +49,6 @@ export const getEligibleZap = pool => {
   };
 };
 
-
 export const computePairAddress = (factoryAddress, pairInitHash, tokenA, tokenB) => {
   const [token0, token1] = sortTokens(tokenA, tokenB);
   return getCreate2Address(
@@ -59,7 +57,6 @@ export const computePairAddress = (factoryAddress, pairInitHash, tokenA, tokenB)
     pairInitHash
   );
 };
-
 
 export const sortTokens = (tokenA, tokenB) => {
   if (tokenA === tokenB)
