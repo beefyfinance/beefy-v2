b0ef929566457c2cb0d76a1b31f7c51fd24a4df0
Sebastian Guido <sebastianezequielguido@gmail.com>
2022-01-13 23:55:05 -0300

    testing custom rpc calling

--- src/features/redux/actions/vault.tsx
+++ src/features/redux/actions/vault.tsx
@@ -16,50 +16,40 @@ import zapAbi from '../../../config/abi/zap.json';
 import uniswapV2PairABI from '../../../config/abi/uniswapV2Pair.json';
 import uniswapV2RouterABI from '../../../config/abi/uniswapV2Router.json';
 
-const getPools = async (items, state, dispatch) => {
-  console.log('redux getPools() processing...');
-  const web3 = state.walletReducer.rpc;
-  const pools = { ...state.vaultReducer.pools };
-  const prices = state.pricesReducer.prices;
-  const apy = state.pricesReducer.apy;
+const getSingleChainPools = async (pools, items, state, key, prices, apy, web3) => {
+  const chain = config[key];
+  const rpc = state.walletReducer.rpc[key];
+  const multicall = new MultiCall(rpc, config[key].multicallAddress);
 
-  if (isEmpty(prices)) {
-    console.log('empty prices :D');
-    return false;
-  }
-
-  const multicall = [];
   const calls = [];
 
-  for (let key in web3) {
-    multicall[key] = new MultiCall(web3[key], config[key].multicallAddress);
-    calls[key] = [];
-  }
+  // console.log(items);
+  // const chainItems = items.filter(item => item.network === key);
 
-  for (let key in items) {
-    const pool = items[key];
+  for (let itemKey in items) {
+    const pool = items[itemKey];
+    if (pool.network !== key) continue;
     if (pool.isGovVault) {
       const tokenContract = new web3[pool.network].eth.Contract(vaultAbi, pool.poolAddress);
       let reqs = {
         id: pool.id,
         totalStaked: tokenContract.methods.totalSupply(),
       };
-      calls[pool.network].push(reqs);
+      calls.push(reqs);
     } else {
       const tokenContract = new web3[pool.network].eth.Contract(vaultAbi, pool.earnedTokenAddress);
-      calls[pool.network].push({
+      calls.push({
         id: pool.id,
         balance: tokenContract.methods.balance(),
         pricePerFullShare: tokenContract.methods.getPricePerFullShare(),
         strategy: tokenContract.methods.strategy(),
       });
     }
-  } //for (let key in items)
+  }
+
 
   const promises = [];
-  for (const key in multicall) {
-    promises.push(multicall[key].all([calls[key]]));
-  }
+  promises.push(multicall.all([calls]));
   const results = await Promise.allSettled(promises);
 
   let response = [];
@@ -100,8 +90,99 @@ const getPools = async (items, state, dispatch) => {
     pools[item.id].tvl = tvl;
     pools[item.id].apy = !isEmpty(apy) && item.id in apy ? apy[item.id] : 0;
     totalTvl = totalTvl.plus(tvl);
-  } //for (let i = 0; i < response.length;
+  }
+  return totalTvl;
+};
 
+const getPools = async (items, state, dispatch) => {
+  console.log('redux getPools() processing...');
+  const web3 = state.walletReducer.rpc;
+  const pools = { ...state.vaultReducer.pools };
+  const prices = state.pricesReducer.prices;
+  const apy = state.pricesReducer.apy;
+
+  if (isEmpty(prices)) {
+    console.log('empty prices :D');
+    return false;
+  }
+
+  const multicall = [];
+  const calls = [];
+
+  let promises = [];
+  for (let key in web3) {
+    promises.push(getSingleChainPools(pools, items, state, key, prices, apy, web3));
+    // multicall[key] = new MultiCall(web3[key], config[key].multicallAddress);
+    // calls[key] = [];
+  }
+  let res = await Promise.all(promises);
+  let totalTvl = res.reduce((tot, cur) => tot.plus(cur));
+
+  // for (let key in items) {
+  //   const pool = items[key];
+  //   if (pool.isGovVault) {
+  //     const tokenContract = new web3[pool.network].eth.Contract(vaultAbi, pool.poolAddress);
+  //     let reqs = {
+  //       id: pool.id,
+  //       totalStaked: tokenContract.methods.totalSupply(),
+  //     };
+  //     calls[pool.network].push(reqs);
+  //   } else {
+  //     const tokenContract = new web3[pool.network].eth.Contract(vaultAbi, pool.earnedTokenAddress);
+  //     calls[pool.network].push({
+  //       id: pool.id,
+  //       balance: tokenContract.methods.balance(),
+  //       pricePerFullShare: tokenContract.methods.getPricePerFullShare(),
+  //       strategy: tokenContract.methods.strategy(),
+  //     });
+  //   }
+  // } //for (let key in items)
+
+  // const promises = [];
+  // for (const key in multicall) {
+  //   promises.push(multicall[key].all([calls[key]]));
+  // }
+  // const results = await Promise.allSettled(promises);
+
+  // let response = [];
+  // results.forEach(result => {
+  //   if (result.status !== 'fulfilled') {
+  //     console.warn('getPoolsAll error', result.reason);
+  //     // FIXME: queue chain retry?
+  //     return;
+  //   }
+  //   response = [...response, ...result.value[0]];
+  // });
+
+  // let totalTvl = new BigNumber(0);
+  // for (let i = 0; i < response.length; i++) {
+  //   const item = response[i];
+
+  //   let tvl;
+
+  //   if (pools[item.id].isGovVault) {
+  //     const totalStaked = new BigNumber(item.totalStaked);
+  //     const price = pools[item.id].oracleId in prices ? prices[pools[item.id].oracleId] : 0;
+  //     tvl = totalStaked
+  //       .times(price)
+  //       .dividedBy(new BigNumber(10).exponentiatedBy(pools[item.id].tokenDecimals));
+  //     if (pools[item.id].excluded) {
+  //       tvl = tvl.minus(pools[pools[item.id].excluded].tvl);
+  //     }
+  //   } else {
+  //     const balance = new BigNumber(item.balance);
+  //     const price = pools[item.id].oracleId in prices ? prices[pools[item.id].oracleId] : 0;
+  //     tvl = balance
+  //       .times(price)
+  //       .dividedBy(new BigNumber(10).exponentiatedBy(pools[item.id].tokenDecimals));
+  //     pools[item.id].pricePerFullShare = item.pricePerFullShare;
+  //     pools[item.id].strategy = item.strategy;
+  //   }
+
+  //   pools[item.id].tvl = tvl;
+  //   pools[item.id].apy = !isEmpty(apy) && item.id in apy ? apy[item.id] : 0;
+  //   totalTvl = totalTvl.plus(tvl);
+  // } //for (let i = 0; i < response.length;
   dispatch({
     type: HOME_FETCH_POOLS_DONE,
     payload: {
@@ -115,25 +196,14 @@ const getPools = async (items, state, dispatch) => {
   return true;
 }; //const getPools = async
 
-const getBoosts = async (items, state, dispatch) => {
-  console.log('redux getBoosts() processing...');
-  const web3 = state.walletReducer.rpc;
-  const prices = state.pricesReducer.prices;
-  const pools = state.vaultReducer.pools;
-  const boosts = { ...state.vaultReducer.boosts };
-
-  const multicall = [];
+const getSingleChainBoosts = async (boosts, pools, items, state, key, web3, prices) => {
+  const multicall = new MultiCall(web3[key], config[key].multicallAddress);
   const calls = [];
   const moos = [];
 
-  for (let key in web3) {
-    multicall[key] = new MultiCall(web3[key], config[key].multicallAddress);
-    calls[key] = [];
-    moos[key] = [];
-  }
-
-  for (let key in items) {
-    const pool = items[key];
+  for (let boostKey in items) {
+    const pool = items[boostKey];
+    if (pool.network !== key) continue;
     const tokenContract = new web3[pool.network].eth.Contract(boostAbi, pool.earnContractAddress);
     calls[pool.network].push({
       id: pool.id,
@@ -147,18 +217,17 @@ const getBoosts = async (items, state, dispatch) => {
         vaultAbi,
         pools[pool.poolId].earnContractAddress
       );
-      moos[pool.network].push({
+      moos.push({
         id: pool.id,
         pricePerFullShare: mooContract.methods.getPricePerFullShare(),
       });
     }
-  } //for (let key in items)
+  }
 
   const promises = [];
-  for (const key in multicall) {
-    promises.push(multicall[key].all([calls[key]]));
-    promises.push(multicall[key].all([moos[key]]));
-  }
+  promises.push(multicall[key].all([calls]));
+  promises.push(multicall[key].all([moos]));
+
   const results = await Promise.allSettled(promises);
   const response = [];
 
@@ -216,7 +285,115 @@ const getBoosts = async (items, state, dispatch) => {
     boost.staked = totalStaked.dividedBy(tokenDecimals);
     boost.tvl = totalStakedInUsd.toFixed(2);
     boost.periodFinish = item.periodFinish;
-  } //for (const key in response)
+  }
+};
+
+const getBoosts = async (items, state, dispatch) => {
+  console.log('redux getBoosts() processing...');
+  const web3 = state.walletReducer.rpc;
+  const prices = state.pricesReducer.prices;
+  const pools = state.vaultReducer.pools;
+  const boosts = { ...state.vaultReducer.boosts };
+
+  const multicall = [];
+  const calls = [];
+  const moos = [];
+
+  const promises = []
+
+  for (let key in web3) {
+    promises.push(getSingleChainBoosts(boosts,pools,items,state,key,web3,prices));
+    // multicall[key] = new MultiCall(web3[key], config[key].multicallAddress);
+    // calls[key] = [];
+    // moos[key] = [];
+  }
+  await Promise.allSettled(promises);
+
+  // for (let key in items) {
+  //   const pool = items[key];
+  //   const tokenContract = new web3[pool.network].eth.Contract(boostAbi, pool.earnContractAddress);
+  //   calls[pool.network].push({
+  //     id: pool.id,
+  //     totalStaked: tokenContract.methods.totalSupply(),
+  //     rewardRate: tokenContract.methods.rewardRate(),
+  //     periodFinish: tokenContract.methods.periodFinish(),
+  //   });
+
+  //   if (pool.isMooStaked) {
+  //     const mooContract = new web3[pool.network].eth.Contract(
+  //       vaultAbi,
+  //       pools[pool.poolId].earnContractAddress
+  //     );
+  //     moos[pool.network].push({
+  //       id: pool.id,
+  //       pricePerFullShare: mooContract.methods.getPricePerFullShare(),
+  //     });
+  //   }
+  // } //for (let key in items)
+
+  // const promises = [];
+  // for (const key in multicall) {
+  //   promises.push(multicall[key].all([calls[key]]));
+  //   promises.push(multicall[key].all([moos[key]]));
+  // }
+  // const results = await Promise.allSettled(promises);
+  // const response = [];
+
+  // results.forEach(result => {
+  //   if (result.status !== 'fulfilled') {
+  //     console.warn('getPoolsAll error', result.reason);
+  //     // FIXME: queue chain retry?
+  //     return;
+  //   }
+
+  //   if (!isEmpty(result.value[0])) {
+  //     result.value[0].forEach(item => {
+  //       if (isEmpty(response[item.id])) {
+  //         response[item.id] = { id: item.id };
+  //       }
+
+  //       if (!isEmpty(item.totalStaked)) {
+  //         response[item.id]['totalStaked'] = item.totalStaked;
+  //       }
+
+  //       if (!isEmpty(item.rewardRate)) {
+  //         response[item.id]['rewardRate'] = item.rewardRate;
+  //       }
+
+  //       if (!isEmpty(item.pricePerFullShare)) {
+  //         response[item.id]['pricePerFullShare'] = item.pricePerFullShare;
+  //       }
+
+  //       if (!isEmpty(item.periodFinish)) {
+  //         response[item.id]['periodFinish'] = item.periodFinish;
+  //       }
+  //     }); //result.value[0].forEach( item
+  //   } //if (!isEmpty( result.value[0]))
+  // }); //results.forEach( result =>
+
+  // for (const key in response) {
+  //   const item = response[key],
+  //     boost = boosts[item.id];
+  //   const tokenDecimals = new BigNumber(10).exponentiatedBy(boost.tokenDecimals);
+  //   const earnedDecimals = new BigNumber(10).exponentiatedBy(boost.earnedTokenDecimals);
+  //   const tokenPrice = boost.tokenOracleId in prices ? prices[boost.tokenOracleId] : 0;
+  //   const earnPrice = boost.earnedOracleId in prices ? prices[boost.earnedOracleId] : 0;
+  //   const totalStaked = boost.isMooStaked
+  //     ? new BigNumber(item.totalStaked).times(item.pricePerFullShare).dividedBy(tokenDecimals)
+  //     : new BigNumber(item.totalStaked);
+  //   const totalStakedInUsd = totalStaked.times(tokenPrice).dividedBy(tokenDecimals);
+  //   const yearlyRewardsInUsd = new BigNumber(item.rewardRate)
+  //     .times(3600)
+  //     .times(24)
+  //     .times(365)
+  //     .times(earnPrice)
+  //     .dividedBy(earnedDecimals);
+
+  //   boost.apr = Number(yearlyRewardsInUsd.dividedBy(totalStakedInUsd));
+  //   boost.staked = totalStaked.dividedBy(tokenDecimals);
+  //   boost.tvl = totalStakedInUsd.toFixed(2);
+  //   boost.periodFinish = item.periodFinish;
+  // } //for (const key in response)
 
   dispatch({
     type: HOME_FETCH_BOOSTS_DONE,
