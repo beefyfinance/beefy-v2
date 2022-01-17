77ca7ec4e9098bd8e1a7e02f136102ea5f56cb45
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-01-17 15:55:20 -0300

    add metis network

---
+++ src/config/zap/metis.tsx
@@ -0,0 +1,9 @@
+export const zaps = [
+  {
+    // Netswap
+    zapAddress: '0xF439B695bB28c9E9865170C1b3e98F5EB4cE9b48',
+    ammRouter: '0x1E876cCe41B7b844FDe09E38Fa1cf00f213bFf56',
+    ammFactory: '0x70f51d68D16e8f9e418441280342BD43AC9Dff9f',
+    ammPairInitHash: '0x966d65068a6a30f10fd1fa814258637a34e059081d79daa94f3e2b6cec48e810',
+  },
+];
