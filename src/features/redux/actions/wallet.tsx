c661b8bf7e13eb4ac148190c90e6a0093803b827
Sebastian Guido <sebastianezequielguido@gmail.com>
2022-01-17 15:06:58 -0300

    cleanup

--- src/features/redux/actions/wallet.tsx
+++ src/features/redux/actions/wallet.tsx
@@ -104,7 +104,6 @@ const connect = () => {
     };
 
     try {
-      console.log(`conneeccttinng`);
       const provider = await state.walletReducer.web3modal.connect();
       const web3 = await new Web3(provider);
       web3.eth.extend({
@@ -116,7 +115,6 @@ const connect = () => {
           },
         ],
       });
-      console.log('heheheh');
       subscribeProvider(provider, web3);
 
       let networkId = await web3.eth.getChainId();
@@ -125,8 +123,6 @@ const connect = () => {
         networkId = 56;
       }
 
-      console.log(state.walletReducer);
-      console.log(state.walletReducer.network);
       if (networkId === config[state.walletReducer.network].chainId) {
         const accounts = await web3.eth.getAccounts();
         // console.log(accounts);
@@ -136,7 +132,6 @@ const connect = () => {
           payload: { address: accounts[0] },
         });
       } else {
-        console.log('connecting to another chain');
         if (!checkNetworkSupport(networkId)) {
           await close();
           if (provider) {
