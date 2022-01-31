4b472c475e6423639e85a78437c871064e79123e
Clément Prévost <prevostclement@gmail.com>
2022-01-31 08:25:34 +0100

    oopsies

--- src/features/home/components/Portfolio/Stats/Stats.tsx
+++ src/features/home/components/Portfolio/Stats/Stats.tsx
@@ -28,36 +28,6 @@ export const Stats = ({ stats, blurred }) => {
 
   const formatStat = value => (empty ? new BigNumber(0).toFixed(0) : formatUsd(value.toNumber()));
 
-  console.log({
-    deposited: stats.deposited.toNumber(),
-    totalYield: stats.totalYield.toNumber(),
-    daily: stats.daily.toNumber(),
-    monthly: stats.monthly.toNumber(),
-  });
-  const portfoliov2 = useSelector((state: BeefyState) => {
-    if (!stats.deposited.isZero()) {
-      //debugger;
-      console.log({
-        selectIsConfigAvailable: selectIsConfigAvailable(state),
-        selectIsPriceAvailable: selectIsPriceAvailable(state),
-      });
-    }
-    return selectUserGlobalStats(state);
-  });
-  console.log({
-    portfoliov1: {
-      deposited: stats.deposited.toNumber(),
-      totalYield: stats.totalYield.toNumber(),
-      daily: stats.daily.toNumber(),
-      monthly: stats.monthly.toNumber(),
-    },
-    postfoliov2: {
-      deposited: portfoliov2.deposited.toNumber(),
-      totalYield: portfoliov2.totalYield.toNumber(),
-      daily: portfoliov2.daily.toNumber(),
-      monthly: portfoliov2.monthly.toNumber(),
-    },
-  });
   return (
     <Grid container className={classes.stats}>
       <Box className={classes.stat}>
