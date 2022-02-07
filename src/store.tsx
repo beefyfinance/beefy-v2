fc7287eea1c3d11726a86466d7a9155864f999c7
Clément Prévost <prevostclement@gmail.com>
2022-02-07 20:37:03 +0100

    Add debug recorder/replayer

--- src/store.tsx
+++ src/store.tsx
@@ -3,6 +3,22 @@ import { persistStore } from 'redux-persist';
 import { rootReducer } from './features/redux/reducers';
 import { loggerMiddleware } from './features/data/middlewares/logger';
 import { walletActionsMiddleware } from './features/data/middlewares/wallet';
+import { debugRecorderMiddleware } from './features/data/middlewares/debug/debug-record';
+import {
+  featureFlag_recordReduxActions,
+  featureFlag_replayReduxActions,
+} from './features/data/utils/feature-flags';
+
+let middlewares = [loggerMiddleware];
+
+if (featureFlag_recordReduxActions()) {
+  middlewares = [debugRecorderMiddleware, ...middlewares];
+}
+
+if (!featureFlag_replayReduxActions) {
+  // don't want this to run actions when replaying
+  middlewares.push(walletActionsMiddleware);
+}
 
 export const store = configureStore({
   reducer: rootReducer,
@@ -14,7 +30,7 @@ export const store = configureStore({
       serializableCheck: false,
       // this makes the old code bug
       immutableCheck: false,
-    }).concat([loggerMiddleware, walletActionsMiddleware]),
+    }).concat(middlewares),
 });
 
 export const persistor = persistStore(store);
