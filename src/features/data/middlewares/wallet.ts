814e641c0747ca3c5a3c7b5fc0b6d437a80a0524
Clément Prévost <prevostclement@gmail.com>
2022-02-10 18:16:33 +0100

    Load balance on connect

--- src/features/data/middlewares/wallet.ts
+++ src/features/data/middlewares/wallet.ts
@@ -8,7 +8,6 @@ import {
   fetchCaptureUserData,
 } from '../actions/scenarios';
 import { ChainEntity } from '../entities/chain';
-import { accountHasChanged, userDidConnect } from '../reducers/wallet';
 import { selectHasWalletBalanceBeenFetched } from '../selectors/balance';
 
 async function reloadUserData(store: BeefyStore) {
@@ -24,15 +23,15 @@ async function reloadUserData(store: BeefyStore) {
 // fetch balance and allowance again
 export function walletActionsMiddleware(store: BeefyStore) {
   return next => async (action: { type: string; payload: { chainId?: ChainEntity['id'] } }) => {
+    const walletAddressBefore = store.getState().user.wallet.address;
     await next(action);
+    const walletAddressAfter = store.getState().user.wallet.address;
 
-    if (action.type === userDidConnect.type || action.type === accountHasChanged.type) {
-      // @ts-ignore
-      const address: string = action.payload.address;
+    if (walletAddressBefore !== walletAddressAfter && walletAddressAfter !== null) {
       // reload data if account has changed and we don't already have some data
       // ie: when user selects a totally new account, we want to fetch
       // his wallet balance and allowances
-      if (!selectHasWalletBalanceBeenFetched(store.getState(), address)) {
+      if (!selectHasWalletBalanceBeenFetched(store.getState(), walletAddressAfter)) {
         setTimeout(() => reloadUserData(store), 50);
       }
     }
