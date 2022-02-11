de8c3fce45b591f652cd52afe1803f71db2e1f2a
Clément Prévost <prevostclement@gmail.com>
2022-02-11 20:53:20 +0100

    Fix price per full share computation

---
+++ src/helpers/ppfs.test.ts
@@ -0,0 +1,66 @@
+import BigNumber from 'bignumber.js';
+import { TokenEntity } from '../features/data/entities/token';
+import { mooAmountToOracleAmount } from './ppfs';
+
+describe('Ppfs tests', () => {
+  it('should properly compute oracle amount of tokens with same decimals: mooVenusBNB', () => {
+    const mooToken: TokenEntity = {
+      buyUrl: null,
+      chainId: 'bsc',
+      contractAddress: '0x6BE4741AB0aD233e4315a10bc783a7B923386b71',
+      decimals: 18,
+      id: 'mooVenusBNB',
+      symbol: 'mooVenusBNB',
+      type: 'erc20',
+    };
+
+    const oracleToken: TokenEntity = {
+      buyUrl: null,
+      chainId: 'bsc',
+      contractAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      decimals: 18,
+      id: 'WBNB',
+      symbol: 'WBNB',
+      type: 'erc20',
+    };
+    const mooAmount = new BigNumber('0.086560461734815659');
+    const ppfs = new BigNumber('1.155580391227848799');
+    const expectedOracleAmount = new BigNumber('0.100027572236381515');
+
+    const actualOracleAmount = mooAmountToOracleAmount(mooToken, oracleToken, ppfs, mooAmount);
+    expect(actualOracleAmount.toPrecision(18).toString()).toBe(
+      expectedOracleAmount.toPrecision(18).toString()
+    );
+  });
+
+  it('should properly compute oracle amount of tokens with different decimals: mooScreamWBTC', () => {
+    const mooToken: TokenEntity = {
+      buyUrl: null,
+      chainId: 'fantom',
+      contractAddress: '0x97927aBfE1aBBE5429cBe79260B290222fC9fbba',
+      decimals: 18,
+      id: 'mooScreamWBTC',
+      symbol: 'mooScreamWBTC',
+      type: 'erc20',
+    };
+
+    const oracleToken: TokenEntity = {
+      buyUrl: null,
+      chainId: 'fantom',
+      contractAddress: '0x321162Cd933E2Be498Cd2267a90534A804051b11',
+      decimals: 8,
+      id: 'WBTC',
+      symbol: 'WBTC',
+      type: 'erc20',
+    };
+
+    const mooAmount = new BigNumber('0.000000000000092138');
+    const ppfs = new BigNumber('1.050525364085333037');
+    const expectedOracleAmount = new BigNumber('0.00096793');
+
+    const actualOracleAmount = mooAmountToOracleAmount(mooToken, oracleToken, ppfs, mooAmount);
+    expect(actualOracleAmount.toPrecision(18).toString()).toBe(
+      expectedOracleAmount.toPrecision(18).toString()
+    );
+  });
+});
