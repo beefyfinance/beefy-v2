4ae2971733306498096fe6af9f370769733e616b
Clément Prévost <prevostclement@gmail.com>
2022-02-27 08:00:01 +0100

    Fix boost apy

--- src/features/data/reducers/boosts.ts
+++ src/features/data/reducers/boosts.ts
@@ -164,24 +164,7 @@ function addBoostToState(
     return;
   }
 
-  let tokenId = apiBoost.earnedToken;
-
-  /**
-   * Fix case for some tokens like "Charge" and "CHARGE"
-   * Only fix when values are different
-   */
-  if (
-    !tokenId.startsWith('moo') &&
-    !tokenId.includes('-') &&
-    !tokenId.includes('_') &&
-    !tokenId.includes('.') &&
-    !tokenId.includes(' ') &&
-    apiBoost.earnedToken !== apiBoost.earnedOracleId &&
-    apiBoost.earnedToken.toLocaleUpperCase() === apiBoost.earnedOracleId.toLocaleUpperCase()
-  ) {
-    tokenId = apiBoost.earnedToken.toLocaleUpperCase();
-  }
-
+  const tokenId = apiBoost.earnedOracleId;
   const boost: BoostEntity = {
     id: apiBoost.id,
     chainId: chainId,
