5f14afdb90be3278c33aa75ba9735bde8ce04533
Clément <998369+prevostc@users.noreply.github.com>
2022-03-02 23:12:09 +0100

    Fix search reset (#475)

--- src/features/data/reducers/filtered-vaults.ts
+++ src/features/data/reducers/filtered-vaults.ts
@@ -5,6 +5,12 @@ import { PlatformEntity } from '../entities/platform';
  * State containing Vault infos
  */
 export type FilteredVaultsState = {
+  /**
+   * Some form element have local copies of the state as putting it inside the
+   * redux store would be too slow for user interactions. This bool tells them
+   * to reset their local copy. The search text is (for now) the only example.
+   **/
+  reseted: boolean;
   sort: 'tvl' | 'apy' | 'safetyScore' | 'default';
   vaultCategory: 'all' | 'featured' | 'stable' | 'bluechip' | 'beefy';
   userCategory: 'all' | 'eligible' | 'deposited';
@@ -17,6 +23,7 @@ export type FilteredVaultsState = {
   onlyBoosted: boolean;
 };
 const initialFilteredVaultsState: FilteredVaultsState = {
+  reseted: true,
   sort: 'default',
   vaultCategory: 'all',
   userCategory: 'all',
@@ -37,33 +44,43 @@ export const filteredVaultsSlice = createSlice({
       return initialFilteredVaultsState;
     },
     setSort(sliceState, action: PayloadAction<FilteredVaultsState['sort']>) {
+      sliceState.reseted = false;
       sliceState.sort = action.payload;
     },
     setVaultCategory(sliceState, action: PayloadAction<FilteredVaultsState['vaultCategory']>) {
+      sliceState.reseted = false;
       sliceState.vaultCategory = action.payload;
     },
     setUserCategory(sliceState, action: PayloadAction<FilteredVaultsState['userCategory']>) {
+      sliceState.reseted = false;
       sliceState.userCategory = action.payload;
     },
     setVaultType(sliceState, action: PayloadAction<FilteredVaultsState['vaultType']>) {
+      sliceState.reseted = false;
       sliceState.vaultType = action.payload;
     },
     setSearchText(sliceState, action: PayloadAction<FilteredVaultsState['searchText']>) {
+      sliceState.reseted = false;
       sliceState.searchText = action.payload;
     },
     setChainIds(sliceState, action: PayloadAction<FilteredVaultsState['chainIds']>) {
+      sliceState.reseted = false;
       sliceState.chainIds = action.payload;
     },
     setPlatformId(sliceState, action: PayloadAction<FilteredVaultsState['platformId']>) {
+      sliceState.reseted = false;
       sliceState.platformId = action.payload;
     },
     setOnlyRetired(sliceState, action: PayloadAction<FilteredVaultsState['onlyRetired']>) {
+      sliceState.reseted = false;
       sliceState.onlyRetired = action.payload;
     },
     setOnlyMoonpot(sliceState, action: PayloadAction<FilteredVaultsState['onlyMoonpot']>) {
+      sliceState.reseted = false;
       sliceState.onlyMoonpot = action.payload;
     },
     setOnlyBoosted(sliceState, action: PayloadAction<FilteredVaultsState['onlyBoosted']>) {
+      sliceState.reseted = false;
       sliceState.onlyBoosted = action.payload;
     },
   },
