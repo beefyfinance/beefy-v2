775dd82953d6035929f3d9efbf7cee95ebc94014
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-03-03 19:46:42 -0300

    smart way to use apikey

--- src/features/vault/components/LaCucinaCard/laCucina.ts
+++ src/features/vault/components/LaCucinaCard/laCucina.ts
@@ -1,12 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
 
+const { REACT_APP_LACUCINA_APIKEY } = process.env;
+
 export class LaCucinaApi {
   public api: AxiosInstance;
   constructor() {
     this.api = axios.create({
       baseURL: 'https://partners.lacucina.io/v2/public',
       timeout: 30 * 1000,
-      headers: { 'x-api-key': process.env.REACT_APP_LACUCINA_APIKEY },
+      headers: { 'x-api-key': REACT_APP_LACUCINA_APIKEY },
     });
   }
   public async getLaCucinaInfo(ovenId: string): Promise<LaCucinaResponse> {
