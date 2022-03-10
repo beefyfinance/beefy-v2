9d2e3521dcd427f93f667966b69aac5f3d5be483
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-03-10 20:35:38 -0300

    update providers to use wallet connect

--- src/features/data/apis/wallet/wallet-connect.ts
+++ src/features/data/apis/wallet/wallet-connect.ts
@@ -306,7 +306,21 @@ function _generateProviderOptions(chain: ChainEntity): Partial<ICoreOptions> {
         description: 'Scan your WalletConnect to Connect',
       },
       options: {
-        rpc: { chainId: chain.rpc },
+        rpc: {
+          56: 'https://bsc-dataseed.binance.org/',
+          43114: 'https://api.avax.network/ext/bc/C/rpc',
+          137: 'https://polygon-rpc.com/',
+          250: 'https://rpc.ftm.tools/',
+          1666600000: 'https://api.harmony.one/',
+          42161: 'https://arb1.arbitrum.io/rpc',
+          1285: 'https://rpc.moonriver.moonbeam.network',
+          42220: 'https://forno.celo.org',
+          25: 'https://evm-cronos.crypto.org/',
+          122: 'https://rpc.fuse.io',
+          1088: 'https://andromeda.metis.io/?owner=1088',
+          1313161554: 'https://mainnet.aurora.dev',
+          1284: 'https://rpc.api.moonbeam.network',
+        },
       },
       package: WalletConnectProvider,
       connector: async (ProviderPackage, options) => {
@@ -325,7 +339,10 @@ function _generateProviderOptions(chain: ChainEntity): Partial<ICoreOptions> {
       },
       package: WalletConnectProvider,
       options: {
-        rpc: { chainId: chain.rpc },
+        rpc: {
+          1: 'https://rpc.fuse.io',
+          122: 'https://rpc.fuse.io',
+        },
       },
       connector: async (ProviderPackage, options) => {
         const provider = new ProviderPackage(options);
