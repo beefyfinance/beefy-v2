da183928e83c19017ba18838aac6e08a1fe13383
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-04-05 22:28:50 +0100

    Variable names

--- src/features/vault/components/StrategyCard/stratText.tsx
+++ src/features/vault/components/StrategyCard/stratText.tsx
@@ -3,29 +3,34 @@ export const stratText = (stratType, platform, assets, want, vamp, t) => {
     case 'StratLP':
     case 'StratMultiLP':
     case 'Vamp':
-      const s_1ST = t(platform === 'Other' ? 'Strat-LP-First-Other' : 'Strat-LP-First', {
+      const firstPart = t(platform === 'Other' ? 'Strat-LP-First-Other' : 'Strat-LP-First', {
         platform: platform,
         LPtoken: want,
       });
-      let s_mid; // eslint-disable-next-line
+      let middlePart = '';
       switch (stratType) {
         case 'StratLP':
-          s_mid = t('Strat-LP', { asset1: assets[0], asset2: assets[1], LPtoken: want });
+          middlePart = t('Strat-LP', { asset1: assets[0], asset2: assets[1], LPtoken: want });
           break;
         case 'StratMultiLP':
-          s_mid = t('Strat-LP-Multi', { LPtoken: want });
+          middlePart = t('Strat-LP-Multi', { LPtoken: want });
           break;
         case 'Vamp':
-          s_mid = t('Strat-LP-Vamp', { subPlatform: platform, topPlatform: vamp, LPtoken: want });
+          middlePart = t('Strat-LP-Vamp', {
+            subPlatform: platform,
+            topPlatform: vamp,
+            LPtoken: want,
+          });
+          break;
       }
-      return s_1ST + ' ' + s_mid + ' ' + t('Strat-LP-GasCost');
+      return firstPart + ' ' + middlePart + ' ' + t('Strat-LP-GasCost');
 
     case 'Lending':
       return t('Strat-Lending', { asset: assets[0] });
 
     case 'SingleStake':
       return (
-        t(platform === 'Other' ? 'Strat-LP-Single-Other' : 'Strat-LP-Single', {
+        t(platform === 'Other' ? 'Strat-Single-Other' : 'Strat-Single', {
           platform: platform,
           token: assets[0],
         }) +
