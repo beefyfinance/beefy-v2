69b5027cf62ade2c8685500aa916a01e5882b8e4
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-04-05 22:46:14 +0100

    Revert "Variable names"
    This reverts commit d7547053c235f2b082fae8e8c73178def2d59915.

--- src/features/vault/components/StrategyCard/stratText.tsx
+++ src/features/vault/components/StrategyCard/stratText.tsx
@@ -3,31 +3,26 @@ export const stratText = (stratType, platform, assets, want, vamp, t) => {
     case 'StratLP':
     case 'StratMultiLP':
     case 'Vamp':
-      const firstPart = t('Strat-LP-First', { platform: platform, LPtoken: want });
-      let middlePart = '';
+      const s_1ST = t('Strat-LP-First', { platform: platform, LPtoken: want });
+      let s_mid; // eslint-disable-next-line
       switch (stratType) {
         case 'StratLP':
-          middlePart = t('Strat-LP', { asset1: assets[0], asset2: assets[1], LPtoken: want });
+          s_mid = t('Strat-LP', { asset1: assets[0], asset2: assets[1], LPtoken: want });
           break;
         case 'StratMultiLP':
-          middlePart = t('Strat-LP-Multi', { LPtoken: want });
+          s_mid = t('Strat-LP-Multi', { LPtoken: want });
           break;
         case 'Vamp':
-          middlePart = t('Strat-LP-Vamp', {
-            subPlatform: platform,
-            topPlatform: vamp,
-            LPtoken: want,
-          });
-          break;
+          s_mid = t('Strat-LP-Vamp', { subPlatform: platform, topPlatform: vamp, LPtoken: want });
       }
-      return firstPart + ' ' + middlePart + ' ' + t('Strat-LP-GasCost');
+      return s_1ST + ' ' + s_mid + ' ' + t('Strat-LP-GasCost');
 
     case 'Lending':
       return t('Strat-Lending', { asset: assets[0] });
 
     case 'SingleStake':
       return (
-        t('Strat-Single', { platform: platform, token: assets[0] }) + ' ' + t('Strat-LP-GasCost')
+        t('Strat-LP-Single', { platform: platform, token: assets[0] }) + ' ' + t('Strat-LP-GasCost')
       );
 
     case 'Maxi':
