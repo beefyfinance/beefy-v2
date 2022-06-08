48c32fc23364db583978d6c32412d08d8d4336e4
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-06-08 10:09:38 +0100

    Enable TUSD withdraws

--- src/features/vault/components/Withdraw/Withdraw.tsx
+++ src/features/vault/components/Withdraw/Withdraw.tsx
@@ -60,7 +60,6 @@ import { styles } from '../styles';
 import { TokenWithDeposit } from '../TokenWithDeposit';
 import { EmeraldGasNotice } from '../EmeraldGasNotice/EmeraldGasNotice';
 import { useAppDispatch, useAppSelector, useAppStore } from '../../../../store';
-import { AlertWarning } from '../../../../components/Alerts';
 
 const useStyles = makeStyles(styles as any);
 
@@ -450,15 +449,6 @@ export const Withdraw = ({ vaultId }: { vaultId: VaultEntity['id'] }) => {
             </Button>
           </Paper>
         </Box>
-        {vault.id === 'scream-tusd' && (
-          <Box mt={3}>
-            <AlertWarning>
-              {t(
-                'There is no liquidity in the underlying protocol to withdraw, withdraws will be activated once more liquidity is available.'
-              )}
-            </AlertWarning>
-          </Box>
-        )}
         <FeeBreakdown
           vault={vault}
           slippageTolerance={formState.slippageTolerance}
@@ -496,11 +486,7 @@ export const Withdraw = ({ vaultId }: { vaultId: VaultEntity['id'] }) => {
                       onClick={handleWithdraw}
                       className={classes.btnSubmit}
                       fullWidth={true}
-                      disabled={
-                        vault.id === 'scream-tusd' ||
-                        formState.amount.isLessThanOrEqualTo(0) ||
-                        !formReady
-                      }
+                      disabled={formState.amount.isLessThanOrEqualTo(0) || !formReady}
                     >
                       {formState.max ? t('Withdraw-All') : t('Withdraw-Verb')}
                     </Button>
@@ -518,11 +504,7 @@ export const Withdraw = ({ vaultId }: { vaultId: VaultEntity['id'] }) => {
                     onClick={handleWithdraw}
                     className={classes.btnSubmit}
                     fullWidth={true}
-                    disabled={
-                      vault.id === 'scream-tusd' ||
-                      formState.amount.isLessThanOrEqualTo(0) ||
-                      !formReady
-                    }
+                    disabled={formState.amount.isLessThanOrEqualTo(0) || !formReady}
                   >
                     {isZapEstimateLoading
                       ? t('Zap-Estimating')
