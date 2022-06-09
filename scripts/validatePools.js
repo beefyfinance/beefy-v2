4a1ea80b085683d3046d7252befe26d8a54aa727
chebiN <17909998+seguido@users.noreply.github.com>
2022-06-08 20:39:41 -0400

    Quicker, parallel validation script

--- scripts/validatePools.js
+++ scripts/validatePools.js
@@ -33,160 +33,178 @@ const oldValidOwners = [
 
 const oldValidFeeRecipients = {};
 
+const addressFields = ['tokenAddress', 'earnedTokenAddress', 'earnContractAddress'];
+
+const allowedEarnSameToken = new Set(['venus-wbnb']);
+
+// Outputs alphabetical list of platforms per chain (useful to make sure they are consistently named)
+const outputPlatformSummary = process.argv.includes('--platform-summary');
+
 const validatePools = async () => {
-  const addressFields = ['tokenAddress', 'earnedTokenAddress', 'earnContractAddress'];
-
-  const allowedEarnSameToken = new Set(['venus-wbnb']);
-
-  // Outputs alphabetical list of platforms per chain (useful to make sure they are consistently named)
-  const outputPlatformSummary = process.argv.includes('--platform-summary');
-
+  
   let exitCode = 0;
-
+  
   let updates = {};
 
   const uniquePoolId = new Set();
 
+  let promises = [];
   for (let [chain, pools] of Object.entries(chainPools)) {
-    console.log(`Validating ${pools.length} pools in ${chain}...`);
-
-    //Governance pools should be separately verified
-    pools = pools.filter(pool => !pool.isGovVault);
-
-    const uniqueEarnedToken = new Set();
-    const uniqueEarnedTokenAddress = new Set();
-    const uniqueOracleId = new Set();
-    const platformCounts = {};
-    let activePools = 0;
-
-    // Populate some extra data.
-    const web3 = new Web3(chainRpcs[chain]);
-    pools = await populateVaultsData(chain, pools, web3);
-    pools = await populateStrategyData(chain, pools, web3);
-
-    pools = override(pools);
-    pools.forEach(pool => {
-      // Errors, should not proceed with build
-      if (uniquePoolId.has(pool.id)) {
-        console.error(`Error: ${pool.id} : Pool id duplicated: ${pool.id}`);
-        exitCode = 1;
-      }
-
-      if (uniqueEarnedToken.has(pool.earnedToken) && !allowedEarnSameToken.has(pool.id)) {
-        console.error(`Error: ${pool.id} : Pool earnedToken duplicated: ${pool.earnedToken}`);
-        exitCode = 1;
-      }
-
-      if (
-        uniqueEarnedTokenAddress.has(pool.earnedTokenAddress) &&
-        !allowedEarnSameToken.has(pool.id)
-      ) {
-        console.error(
-          `Error: ${pool.id} : Pool earnedTokenAddress duplicated: ${pool.earnedTokenAddress}`
-        );
-        exitCode = 1;
-      }
-
-      if (pool.earnedTokenAddress !== pool.earnContractAddress) {
-        console.error(
-          `Error: ${pool.id} : Pool earnedTokenAddress not same as earnContractAddress: ${pool.earnedTokenAddress} != ${pool.earnContractAddress}`
-        );
-        exitCode = 1;
-      }
-
-      if (!pool.tokenDescription) {
-        console.error(
-          `Error: ${pool.id} : Pool tokenDescription missing - required for UI: vault card`
-        );
-        exitCode = 1;
-      }
-
-      if (!pool.platform) {
-        console.error(
-          `Error: ${pool.id} : Pool platform missing - required for UI: filter (Use 'Other' if necessary)`
-        );
-        exitCode = 1;
-      } else {
-        platformCounts[pool.platform] = platformCounts.hasOwnProperty(pool.platform)
-          ? platformCounts[pool.platform] + 1
-          : 1;
-      }
-
-      if (!pool.createdAt) {
-        console.error(
-          `Error: ${pool.id} : Pool createdAt timestamp missing - required for UI: vault sorting`
-        );
-        exitCode = 1;
-      } else if (isNaN(pool.createdAt)) {
-        console.error(
-          `Error: ${pool.id} : Pool createdAt timestamp wrong type, should be a number`
-        );
-        exitCode = 1;
-      }
-
-      addressFields.forEach(field => {
-        if (pool.hasOwnProperty(field) && !isValidChecksumAddress(pool[field])) {
-          const maybeValid = maybeChecksumAddress(pool[field]);
-          console.error(
-            `Error: ${pool.id} : ${field} requires checksum - ${
-              maybeValid ? `\n\t${field}: '${maybeValid}',` : 'it is invalid'
-            }`
-          );
-          exitCode = 1;
-        }
-      });
-
-      if (pool.status === 'active') {
-        activePools++;
-      }
-
-      if (new BigNumber(pool.totalSupply).isZero()) {
-        if (pool.status !== 'eol') {
-          console.error(`Error: ${pool.id} : Pool is empty`);
-          exitCode = 1;
-          if (!('emptyVault' in updates)) updates['emptyVault'] = {};
-          updates.emptyVault[pool.id] = pool.earnContractAddress;
-        } else {
-          console.warn(`${pool.id} : eol pool is empty`);
-        }
-      }
-
-      uniquePoolId.add(pool.id);
-      uniqueEarnedToken.add(pool.earnedToken);
-      uniqueEarnedTokenAddress.add(pool.earnedTokenAddress);
-      uniqueOracleId.add(pool.oracleId);
-
-      const { keeper, strategyOwner, vaultOwner, beefyFeeRecipient } =
-        addressBook[chain].platforms.beefyfinance;
-
-      updates = isKeeperCorrect(pool, chain, keeper, updates);
-      updates = isStratOwnerCorrect(pool, chain, strategyOwner, updates);
-      updates = isVaultOwnerCorrect(pool, chain, vaultOwner, updates);
-      updates = isBeefyFeeRecipientCorrect(pool, chain, beefyFeeRecipient, updates);
-    });
-    if (!isEmpty(updates)) {
-      exitCode = 1;
-    }
-
-    if (outputPlatformSummary) {
-      console.log(
-        `Platforms: \n${Object.entries(platformCounts)
-          .sort(([platformA], [platformB]) =>
-            platformA.localeCompare(platformB, 'en', { sensitivity: 'base' })
-          )
-          .map(([platform, count]) => `\t${platform} (${count})`)
-          .join('\n')}`
-      );
-    }
-
-    console.log(`Active pools: ${activePools}/${pools.length}\n`);
+    promises.push(validateSingleChain(chain, pools, uniquePoolId));
   }
+  let results = await Promise.all(promises);
 
+  exitCode = results.reduce((acum, cur) => acum + cur.exitCode > 0 ? 1 : 0, 0);
+  results.forEach(res => {
+    if (!isEmpty(res.updates)) {
+      updates[res.chain] = updates;
+    }
+  });
   // Helpful data structures to correct addresses.
   console.log('Required updates.', JSON.stringify(updates));
 
   return exitCode;
 };
+
+const validateSingleChain = async (chain, pools, uniquePoolId) => {
+  console.log(`Validating ${pools.length} pools in ${chain}...`);
+
+  let updates = {};
+  let exitCode = 0;
+  //Governance pools should be separately verified
+  pools = pools.filter(pool => !pool.isGovVault);
+
+  const uniqueEarnedToken = new Set();
+  const uniqueEarnedTokenAddress = new Set();
+  const uniqueOracleId = new Set();
+  const platformCounts = {};
+  let activePools = 0;
+
+  // Populate some extra data.
+  const web3 = new Web3(chainRpcs[chain]);
+  pools = await populateVaultsData(chain, pools, web3);
+  pools = await populateStrategyData(chain, pools, web3);
+
+  pools = override(pools);
+  pools.forEach(pool => {
+    // Errors, should not proceed with build
+    if (uniquePoolId.has(pool.id)) {
+      console.error(`Error: ${pool.id} : Pool id duplicated: ${pool.id}`);
+      exitCode = 1;
+    }
+
+    if (uniqueEarnedToken.has(pool.earnedToken) && !allowedEarnSameToken.has(pool.id)) {
+      console.error(`Error: ${pool.id} : Pool earnedToken duplicated: ${pool.earnedToken}`);
+      exitCode = 1;
+    }
+
+    if (
+      uniqueEarnedTokenAddress.has(pool.earnedTokenAddress) &&
+      !allowedEarnSameToken.has(pool.id)
+    ) {
+      console.error(
+        `Error: ${pool.id} : Pool earnedTokenAddress duplicated: ${pool.earnedTokenAddress}`
+      );
+      exitCode = 1;
+    }
+
+    if (pool.earnedTokenAddress !== pool.earnContractAddress) {
+      console.error(
+        `Error: ${pool.id} : Pool earnedTokenAddress not same as earnContractAddress: ${pool.earnedTokenAddress} != ${pool.earnContractAddress}`
+      );
+      exitCode = 1;
+    }
+
+    if (!pool.tokenDescription) {
+      console.error(
+        `Error: ${pool.id} : Pool tokenDescription missing - required for UI: vault card`
+      );
+      exitCode = 1;
+    }
+
+    if (!pool.platform) {
+      console.error(
+        `Error: ${pool.id} : Pool platform missing - required for UI: filter (Use 'Other' if necessary)`
+      );
+      exitCode = 1;
+    } else {
+      platformCounts[pool.platform] = platformCounts.hasOwnProperty(pool.platform)
+        ? platformCounts[pool.platform] + 1
+        : 1;
+    }
+
+    if (!pool.createdAt) {
+      console.error(
+        `Error: ${pool.id} : Pool createdAt timestamp missing - required for UI: vault sorting`
+      );
+      exitCode = 1;
+    } else if (isNaN(pool.createdAt)) {
+      console.error(
+        `Error: ${pool.id} : Pool createdAt timestamp wrong type, should be a number`
+      );
+      exitCode = 1;
+    }
+
+    addressFields.forEach(field => {
+      if (pool.hasOwnProperty(field) && !isValidChecksumAddress(pool[field])) {
+        const maybeValid = maybeChecksumAddress(pool[field]);
+        console.error(
+          `Error: ${pool.id} : ${field} requires checksum - ${
+            maybeValid ? `\n\t${field}: '${maybeValid}',` : 'it is invalid'
+          }`
+        );
+        exitCode = 1;
+      }
+    });
+
+    if (pool.status === 'active') {
+      activePools++;
+    }
+
+    if (new BigNumber(pool.totalSupply).isZero()) {
+      if (pool.status !== 'eol') {
+        console.error(`Error: ${pool.id} : Pool is empty`);
+        exitCode = 1;
+        if (!('emptyVault' in updates)) updates['emptyVault'] = {};
+        updates.emptyVault[pool.id] = pool.earnContractAddress;
+      } else {
+        console.warn(`${pool.id} : eol pool is empty`);
+      }
+    }
+
+    uniquePoolId.add(pool.id);
+    uniqueEarnedToken.add(pool.earnedToken);
+    uniqueEarnedTokenAddress.add(pool.earnedTokenAddress);
+    uniqueOracleId.add(pool.oracleId);
+
+    const { keeper, strategyOwner, vaultOwner, beefyFeeRecipient } =
+      addressBook[chain].platforms.beefyfinance;
+
+    updates = isKeeperCorrect(pool, chain, keeper, updates);
+    updates = isStratOwnerCorrect(pool, chain, strategyOwner, updates);
+    updates = isVaultOwnerCorrect(pool, chain, vaultOwner, updates);
+    updates = isBeefyFeeRecipientCorrect(pool, chain, beefyFeeRecipient, updates);
+  });
+  if (!isEmpty(updates)) {
+    exitCode = 1;
+  }
+
+  if (outputPlatformSummary) {
+    console.log(
+      `Platforms: \n${Object.entries(platformCounts)
+        .sort(([platformA], [platformB]) =>
+          platformA.localeCompare(platformB, 'en', { sensitivity: 'base' })
+        )
+        .map(([platform, count]) => `\t${platform} (${count})`)
+        .join('\n')}`
+    );
+  }
+
+  console.log(`${chain} active pools: ${activePools}/${pools.length}\n`);
+  
+  return { chain, exitCode, updates };
+}
+
 
 // Validation helpers. These only log for now, could throw error if desired.
 const isKeeperCorrect = (pool, chain, chainKeeper, updates) => {
