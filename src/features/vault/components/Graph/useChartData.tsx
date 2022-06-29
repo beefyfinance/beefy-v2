8284e7b8cba32b39d5900b3b5cfc8eb225a3d6a9
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-06-29 18:20:13 -0300

    remove unused import

--- src/features/vault/components/Graph/useChartData.tsx
+++ src/features/vault/components/Graph/useChartData.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 import { getBeefyApi } from '../../../data/apis/instances';
 import { config } from '../../../../config/config';
 import { max, sum } from 'lodash';
