fe6bfc258a706096b23e9fbabfa22e7eae1f710d
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-06-30 13:32:07 -0300

    add lpProviderFee

--- src/config/zap/cronos.tsx
+++ src/config/zap/cronos.tsx
@@ -5,6 +5,7 @@ export const zaps = [
     ammRouter: '0x145863Eb42Cf62847A6Ca784e6416C1682b1b2Ae',
     ammFactory: '0x3B44B2a187a7b3824131F8db5a74194D0a42Fc15',
     ammPairInitHash: '0xa77ee1cc0f39570ddde947459e293d7ebc2c30ff4e8fc45860afdcb2c2d3dc17',
+    lpProviderFee: 0.003,
   },
   {
     // CronaSwap
@@ -12,5 +13,6 @@ export const zaps = [
     ammRouter: '0xcd7d16fB918511BF7269eC4f48d61D79Fb26f918',
     ammFactory: '0x73A48f8f521EB31c55c0e1274dB0898dE599Cb11',
     ammPairInitHash: '0xc93158cffa5b575e32566e81e847754ce517f8fa988d3e25cf346d916216e06f',
+    lpProviderFee: 0.0025,
   },
 ];
--- src/config/zap/fantom.tsx
+++ src/config/zap/fantom.tsx
@@ -5,6 +5,7 @@ export const zaps = [
     ammRouter: '0xF491e7B69E4244ad4002BC14e878a34207E38c29',
     ammFactory: '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3',
     ammPairInitHash: '0xcdf2deca40a0bd56de8e3ce5c7df6727e5b1bf2ac96f283fa9c4b3e6b42ea9d2',
+    lpProviderFee: 0.002,
   },
   {
     // JetSwap
@@ -12,6 +13,7 @@ export const zaps = [
     ammRouter: '0x845E76A8691423fbc4ECb8Dd77556Cb61c09eE25',
     ammFactory: '0xf6488205957f0b4497053d6422F49e27944eE3Dd',
     ammPairInitHash: '0xa5e6089ea250dac750e4867fc4ce7f2a864bd94446564351fe9329f378963974',
+    lpProviderFee: 0.001,
   },
   {
     // SpiritSwap
@@ -19,6 +21,7 @@ export const zaps = [
     ammRouter: '0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52',
     ammFactory: '0xEF45d134b73241eDa7703fa787148D9C9F4950b0',
     ammPairInitHash: '0xe242e798f6cee26a9cb0bbf24653bf066e5356ffeac160907fe2cc108e238617',
+    lpProviderFee: 0.003,
   },
   {
     // Sushi
@@ -26,6 +29,7 @@ export const zaps = [
     ammRouter: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
     ammFactory: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
     ammPairInitHash: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+    lpProviderFee: 0.003,
   },
   {
     // WigoSwap
@@ -33,6 +37,7 @@ export const zaps = [
     ammRouter: '0x5023882f4D1EC10544FCB2066abE9C1645E95AA0',
     ammFactory: '0xC831A5cBfb4aC2Da5ed5B194385DFD9bF5bFcBa7',
     ammPairInitHash: '0x55c39e9406ff3c89a193882b4752879e73c8a0ce1222fe1de34c5e8f6482d9b6',
+    lpProviderFee: 0.0019,
   },
   {
     // TombSwap
@@ -40,5 +45,6 @@ export const zaps = [
     ammRouter: '0x6D0176C5ea1e44b08D3dd001b0784cE42F47a3A7',
     ammFactory: '0xE236f6890F1824fa0a7ffc39b1597A5A6077Cfe9',
     ammPairInitHash: '0x2dfbcf1b907f911bc66d083d103a1d7de0b8b21a6cb2a66a78d1f1559018fba4',
+    lpProviderFee: 0,
   },
 ];
