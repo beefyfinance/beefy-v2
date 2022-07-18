e2ba46fc631ebcb23ff446cc848f94fa6941d098
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-07-18 17:53:55 -0300

    sort by asc tvl modal

--- src/features/home/components/Portfolio/ModalTvl/ModalTvl.tsx
+++ src/features/home/components/Portfolio/ModalTvl/ModalTvl.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '../../../../vault/comp
 import CloseIcon from '@material-ui/icons/Close';
 import { styles } from './styles';
 import { useTranslation } from 'react-i18next';
-import { selectAllChains } from '../../../../data/selectors/chains';
+import { selectChainById } from '../../../../data/selectors/chains';
 import { ChainEntity } from '../../../../data/entities/chain';
 import { selectTvlByChain } from '../../../../data/selectors/tvl';
 import BigNumber from 'bignumber.js';
@@ -12,6 +12,7 @@ import { formatBigUsd } from '../../../../../helpers/format';
 import { ContentLoading } from '../../../../../components/ContentLoading';
 import { Button } from '../../../../../components/Button';
 import { useAppSelector } from '../../../../../store';
+import { sortBy } from 'lodash';
 
 const useStyles = makeStyles(styles);
 
@@ -24,7 +25,14 @@ const _ModalTvl = forwardRef<HTMLDivElement, ModalTvlProps>(function ({ close },
   const { t } = useTranslation();
   const tvls = useAppSelector(selectTvlByChain);
 
-  const chains = useAppSelector(selectAllChains);
+  const sortedTvls = React.useMemo(() => {
+    const list = [];
+    for (const [chainId, tvl] of Object.entries(tvls)) {
+      list.push({ tvl: tvl.toNumber(), chainId });
+    }
+    return sortBy(list, ['tvl']).reverse();
+  }, [tvls]);
+
   return (
     <div className={classes.holder} ref={ref} tabIndex={-1}>
       <Card>
@@ -36,10 +44,10 @@ const _ModalTvl = forwardRef<HTMLDivElement, ModalTvlProps>(function ({ close },
         </CardHeader>
         <CardContent className={classes.container}>
           <Grid container spacing={2}>
-            {chains.map(chain => {
+            {sortedTvls.map((item: any) => {
               return (
-                <Grid key={chain.id} item xs={6} lg={3}>
-                  <Chain chain={chain} tvl={tvls[chain.id]} />
+                <Grid key={item.chainId} item xs={6} lg={3}>
+                  <Chain chain={item.chainId} tvl={tvls[item.chainId]} />
                 </Grid>
               );
             })}
@@ -55,18 +63,19 @@ const _ModalTvl = forwardRef<HTMLDivElement, ModalTvlProps>(function ({ close },
 
 export const ModalTvl = memo<ModalTvlProps>(_ModalTvl);
 
-function Chain({ chain, tvl }: { chain: ChainEntity; tvl: BigNumber }) {
+function Chain({ chain, tvl }: { chain: ChainEntity['id']; tvl: BigNumber }) {
   const classes = useStyles();
+  const _chain = useAppSelector(state => selectChainById(state, chain));
 
   return (
     <Box className={classes.chain}>
       <img
         className={classes.chainLogo}
-        alt={chain.id}
-        src={require(`../../../../../images/networks/${chain.id}.svg`).default}
+        alt={_chain.id}
+        src={require(`../../../../../images/networks/${_chain.id}.svg`).default}
       />
       <Box>
-        <div className={classes.chainText}>{chain.name}</div>
+        <div className={classes.chainText}>{_chain.name}</div>
         <>
           {tvl ? <div className={classes.chainValue}>{formatBigUsd(tvl)}</div> : <ContentLoading />}
         </>
