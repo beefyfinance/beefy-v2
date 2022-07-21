9f3b71a4c7920760faa24c7c25b0b73508135874
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-07-21 14:59:44 -0300

    remove unused useEffect

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo } from 'react';
 import { Container, makeStyles } from '@material-ui/core';
 import { Filters } from './components/Filters';
 import { Portfolio } from './components/Portfolio';
