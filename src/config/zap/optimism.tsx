ae3304220e7332af7d3a7d9c666bf64ae2674d1c
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-07-25 15:02:34 +0100

    Update zap contract

--- src/config/zap/optimism.tsx
+++ src/config/zap/optimism.tsx
@@ -2,7 +2,7 @@ export const zaps = [
   {
     // velodrome
     type: 'solidly',
-    zapAddress: '0x9da5A072804eE44B6259181BdE3F5393aEEf2bEC',
+    zapAddress: '0xD8b3cca9fe905497c22d500Cdf76BEdA0a65d442',
     ammRouter: '0xa132DAB612dB5cB9fC9Ac426A0Cc215A3423F9c9',
     ammFactory: '0x25CbdDb98b35ab1FF77413456B31EC81A6B6B746',
     ammPairInitHash: '0xc1ac28b1c4ebe53c0cff67bab5878c4eb68759bb1e9f73977cd266b247d149f0',
