ba7f681965f1ecd505af63de8ad694f6f7773718
MirthFutures <mirthfutures@gmail.com>
2022-08-20 10:40:03 -0400

    Better Velodrome Zap

--- src/config/zap/optimism.tsx
+++ src/config/zap/optimism.tsx
@@ -2,7 +2,7 @@ export const zaps = [
   {
     // velodrome
     type: 'solidly',
-    zapAddress: '0xD8b3cca9fe905497c22d500Cdf76BEdA0a65d442',
+    zapAddress: '0xE4b3C2b19Db4FbaAfbE99bd8Ec869fC4C37063ae',
     ammRouter: '0xa132DAB612dB5cB9fC9Ac426A0Cc215A3423F9c9',
     ammFactory: '0x25CbdDb98b35ab1FF77413456B31EC81A6B6B746',
     ammPairInitHash: '0xc1ac28b1c4ebe53c0cff67bab5878c4eb68759bb1e9f73977cd266b247d149f0',
