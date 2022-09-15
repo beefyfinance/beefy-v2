38700ff765b96b7b7366b6eaddda53c6a66eb7d7
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-09-15 10:03:54 -0300

    update bridge button on vault page

--- src/features/vault/components/VaultBuyLinks/VaultBuyLinks.tsx
+++ src/features/vault/components/VaultBuyLinks/VaultBuyLinks.tsx
@@ -5,6 +5,7 @@ import { selectVaultById } from '../../../data/selectors/vaults';
 import { styles } from './styles';
 import { useAppSelector } from '../../../../store';
 import { LinkButton } from '../../../../components/LinkButton';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(styles);
 
@@ -19,6 +20,11 @@ export function VaultBuyLinks({ vaultId }: { vaultId: VaultEntity['id'] }) {
       {vault.buyTokenUrl && <LinkButton href={vault.buyTokenUrl} text={t('Transact-BuyTkn')} />}
       {vault.addLiquidityUrl && (
         <LinkButton href={vault.addLiquidityUrl} text={t('Transact-AddLiquidity')} />
+      )}
+      {vault.assetIds.includes('BIFI') && (
+        <Link to="/bridge" className={classes.btnSecondary}>
+          {t('Header-BridgeBifi')}
+        </Link>
       )}
     </div>
   );
