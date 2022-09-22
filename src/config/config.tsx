0a9304f99a3001bb98abd68659297cfb3c616cb3
kexley <sircryptomaxi@gmail.com>
2022-09-22 10:56:30 -0400

    Add Arb Stargate

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -242,7 +242,7 @@ export const config = {
       rpcUrls: ['https://arb1.arbitrum.io/rpc'],
       blockExplorerUrls: ['https://arbiscan.io/'],
     },
-    stableCoins: ['USDC', 'USDT', 'MIM', 'VST', 'DAI'],
+    stableCoins: ['USDC', 'USDT', 'MIM', 'VST', 'DAI', 'sarUSDC', 'sarUSDT'],
   },
   celo: {
     name: 'Celo',
