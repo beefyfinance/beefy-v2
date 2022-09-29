2feb94a08242822fb66db3bfac0c39cc2eb8a368
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-09-29 17:16:08 -0300

    update insurace and leverage title style

--- src/features/vault/components/InsuranceCards/styles.tsx
+++ src/features/vault/components/InsuranceCards/styles.tsx
@@ -3,7 +3,7 @@ import { Theme } from '@material-ui/core/styles';
 export const styles = (theme: Theme) => ({
   container: { marginTop: '24px' },
   title: {
-    ...theme.typography.h3,
+    ...theme.typography.h2,
     color: theme.palette.text.primary,
   },
 });
