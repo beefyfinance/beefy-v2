b9d94f50e6c8647c0331c086688d68e73c2c2c6d
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-10-27 16:36:24 +0100

    Add glp countdown (#1136)
    * Add GMX & GLP
    
    * Update addresses
    
    * Swap moment.js for date-fns
    
    * Add glp withdraw countdown
    
    Co-authored-by: kexley <sircryptomaxi@gmail.com>

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,3 +1,6 @@
+import { intervalToDuration } from 'date-fns';
+import { zeroPad } from './format';
+
 export function datesAreEqual(a: Date | null, b: Date | null): boolean {
   // both are the same Date object, or both are null
   if (a === b) {
@@ -10,4 +13,24 @@ export function datesAreEqual(a: Date | null, b: Date | null): boolean {
   }
 
   return a.getTime() === b.getTime();
+}
+
+export function formatTimeUntil(when: Date, maxParts = 3, minParts = 3): string {
+  const parts: (keyof Duration)[] = ['years', 'months', 'days', 'hours', 'minutes', 'seconds'];
+  const numParts = parts.length;
+  const duration = intervalToDuration({ start: new Date(), end: when });
+
+  for (let i = 0; i < numParts - minParts; ++i) {
+    if (duration[parts[i]]) {
+      return parts
+        .slice(i, i + maxParts)
+        .map(part => `${zeroPad(duration[part])}${part[0]}`)
+        .join(' ');
+    }
+  }
+
+  return parts
+    .slice(numParts - minParts)
+    .map(part => `${zeroPad(duration[part] || 0)}${part[0]}`)
+    .join(' ');
 }
