0f40b3dea4bd5a6fc9c1c47cf3c169371dda4270
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-11-05 01:28:33 +0000

    horizontal scroller

--- src/features/dashboard/components/UserVaults/styles.tsx
+++ src/features/dashboard/components/UserVaults/styles.tsx
@@ -1,4 +1,9 @@
 import { Theme } from '@material-ui/core';
+
 export const styles = (theme: Theme) => ({
-  tablesContainer: { display: 'grid', gridTemplateColums: '1fr', rowGap: '24px' },
+  tablesContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'minmax(0, 1fr)',
+    rowGap: '24px',
+  },
 });
