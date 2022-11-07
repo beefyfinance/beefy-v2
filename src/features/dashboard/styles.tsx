3155613a156afa23af966cefc939650107da0e1a
erenjaegar77 <eerenjaegar77@protonmail.com>
2022-11-07 16:21:07 -0300

    fix random white space before footer

--- src/features/dashboard/styles.tsx
+++ src/features/dashboard/styles.tsx
@@ -2,6 +2,7 @@ import { Theme } from '@material-ui/core';
 
 export const styles = (theme: Theme) => ({
   dashboard: {
+    minHeight: '70vh',
     backgroundColor: '#121420',
     paddingBottom: '48px',
   },
