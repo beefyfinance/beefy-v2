669027ee0aadf6187437f490de82b5b0cfce9f34
schnarkus <markus.simianer@yahoo.de>
2022-12-09 23:33:43 +0100

    Add fish vaults, change MIM and FRAX to MCAP_LARGE

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -247,7 +247,7 @@ export const config = {
       rpcUrls: ['https://arb1.arbitrum.io/rpc'],
       blockExplorerUrls: ['https://arbiscan.io/'],
     },
-    stableCoins: ['USDC', 'USDT', 'MIM', 'VST', 'DAI', 'sarUSDC', 'sarUSDT', 'FRAX'],
+    stableCoins: ['USDC', 'USDT', 'MIM', 'VST', 'DAI', 'sarUSDC', 'sarUSDT', 'FRAX', 'USX', 'MAI'],
   },
   avax: {
     name: 'Avalanche',
