ec969bc96bea46b1491cb34b185a41c873b10253
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-12-12 14:51:28 +0000

    Fix ETH on polygon

--- src/config/zap/one-inch.ts
+++ src/config/zap/one-inch.ts
@@ -4,9 +4,9 @@ export const zaps: OneInchZapConfig[] = [
   {
     zapAddress: '0xd2154257Ec830610b609b13056340564f85F2f86',
     chainId: 'polygon',
-    depositFromTokens: ['MATIC', 'WMATIC', 'USDC', 'USDT', 'DAI', 'WETH', 'WBTC'],
+    depositFromTokens: ['MATIC', 'WMATIC', 'USDC', 'USDT', 'DAI', 'ETH', 'WBTC'],
     blockedDepositToTokens: ['S*USDC', 'S*USDT', 'beQI'],
-    withdrawToTokens: ['MATIC', 'BIFI', 'USDC', 'USDT', 'DAI', 'WETH', 'WBTC'],
+    withdrawToTokens: ['MATIC', 'BIFI', 'USDC', 'USDT', 'DAI', 'ETH', 'WBTC'],
     blockedWithdrawFromTokens: ['S*USDC', 'S*USDT', 'beQI'],
   },
   {
