47227b39092a12700b033d623721d9da300623d4
Alevo <nautadev@gmail.com>
2022-12-20 18:23:45 +0100

    Merged the fix from ReflectiveChimp

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -3,6 +3,6 @@ import { ElementType } from 'react';
 
 type RouterProps = BrowserRouterProps & HashRouterProps;
 
-export const routerMode = process.env.REACT_APP_ROUTER === 'browser' ? 'browser' : 'hash';
+export const routerMode : 'hash' | 'browser' = process.env.REACT_APP_ROUTER === 'browser' ? 'browser' : 'hash';
 export const Router: ElementType<RouterProps> =
   routerMode === 'browser' ? BrowserRouter : HashRouter;
