c6aa04b28939de4ec31af40b8d9585c68e7972f9
erenjaegar77 <eerenjaegar77@protonmail.com>
2023-01-13 15:04:58 -0300

    zap banner

--- src/components/Banners/AnnouncementBanner/AnnouncementBanner.tsx
+++ src/components/Banners/AnnouncementBanner/AnnouncementBanner.tsx
@@ -3,14 +3,13 @@ import { makeStyles } from '@material-ui/core';
 import { styles } from './styles';
 import { useLocalStorageBoolean } from '../../../helpers/useLocalStorageBoolean';
 import { Banner } from '../Banner';
-import TreasuryIcon from '../../../images/icons/beefy-treasury.svg';
-import { Link } from 'react-router-dom';
+import ZapIcon from '../../../images/bifi-logos/bifi-zap-v2.svg';
 
 const useStyles = makeStyles(styles);
 
 export const AnnouncementBanner = memo(function AnnouncementBanner() {
   const classes = useStyles();
-  const [hideBanner, setHideBanner] = useLocalStorageBoolean('hideTreasuryBanner', false);
+  const [hideBanner, setHideBanner] = useLocalStorageBoolean('hideZapBanner', false);
 
   const closeBanner = useCallback(() => {
     setHideBanner(true);
@@ -22,15 +21,20 @@ export const AnnouncementBanner = memo(function AnnouncementBanner() {
 
   return (
     <Banner
-      icon={<img className={classes.icon} src={TreasuryIcon} alt="treasury" />}
+      icon={<img className={classes.icon} src={ZapIcon} alt="treasury" />}
       text={
         <>
-          Introducing{' '}
-          <Link to="/treasury" className={classes.link}>
-            Treasury dashboard.
-          </Link>{' '}
-          Explore our latest UI addition for trustworthy insights into Beefy’s treasury and
-          financials.
+          Zap into the future with the all-new{' '}
+          <a
+            href="https://beefy.com/articles/revolutionizing-beefy-zap-in-partnership-with-1inch/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes.link}
+          >
+            Beefy Zap.
+          </a>{' '}
+          From blue chips, natives and stables to Beefy vaults in just one transaction, powered by
+          1inch.
         </>
       }
       onClose={closeBanner}
