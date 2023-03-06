22f350dfd22217440c5c7e2bb33be44248798349
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-03-06 16:04:50 +0000

    Revert "Fix beFTM Minter (#1338)"
    This reverts commit cbbcc91e831e1afdaf016e78be30d5ca920fb1b5.

--- src/features/data/actions/wallet-actions.tsx
+++ src/features/data/actions/wallet-actions.tsx
@@ -66,7 +66,7 @@ import { AmmEntity } from '../entities/amm';
 import { WANT_TYPE } from '../apis/amm/types';
 import { getVaultWithdrawnFromContract } from '../apis/transact/helpers/vault';
 import { swapWithFee } from '../apis/transact/helpers/one-inch';
-// import { selectOneInchZapByChainId } from '../selectors/zap';
+import { selectOneInchZapByChainId } from '../selectors/zap';
 
 export const WALLET_ACTION = 'WALLET_ACTION';
 export const WALLET_ACTION_RESET = 'WALLET_ACTION_RESET';
@@ -1203,28 +1203,12 @@ const mintDeposit = (
     const amountInWei = toWei(amount, payToken.decimals);
     const amountInWeiString = amountInWei.toString(10);
     const isNative = isTokenNative(payToken);
-    // TEMPFIX
-    const oneInchPriceOracles: Record<ChainEntity['id'], string> = {
-      polygon: '0x7F069df72b7A39bCE9806e3AfaF579E54D8CF2b9',
-      fantom: '0xE8E598A1041b6fDB13999D275a202847D9b654ca',
-      optimism: '0x11DEE30E710B8d4a8630392781Cc3c0046365d4c',
-      avax: '0xBd0c7AaF0bF082712EbE919a9dD94b2d978f79A9',
-      arbitrum: '0x735247fb0a604c0adC6cab38ACE16D0DbA31295F',
-      bsc: '0xfbD61B037C325b959c0F6A7e69D8f37770C2c550',
-      aurora: '0xE4E0552452e5cC1306A2bF5B2Fd9b1eA19418795',
-      ethereum: '0x07D91f5fb9Bf7798734C3f606dB065549F6893bb',
-    };
 
     const buildCall = async () => {
       if (canZapInWithOneInch) {
         const swapInToken = isNative ? selectChainWrappedNativeToken(state, chainId) : payToken;
-        // const zap = selectOneInchZapByChainId(state, chain.chainId);
-        const priceOracleAddress = oneInchPriceOracles[chainId];
-        if (!priceOracleAddress) {
-          throw new Error(`No price oracle found for chain ${chainId}`);
-        }
-
-        const oneInchApi = await getOneInchApi(chain, priceOracleAddress);
+        const zap = selectOneInchZapByChainId(state, chain.chainId);
+        const oneInchApi = await getOneInchApi(chain, zap.priceOracleAddress);
         const swapData = await oneInchApi.getSwap({
           disableEstimate: true, // otherwise will fail due to no allowance
           fromAddress: contractAddress,
