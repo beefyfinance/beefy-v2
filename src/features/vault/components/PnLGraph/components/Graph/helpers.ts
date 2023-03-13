ed65daba02cd67c7df68ef482f010513f8b46121
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-03-13 20:39:46 +0000

    Use e notation under 0.001

--- src/features/vault/components/PnLGraph/components/Graph/helpers.ts
+++ src/features/vault/components/PnLGraph/components/Graph/helpers.ts
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import { TimeBucketType } from '../../../../../data/apis/analytics/analytics-types';
+import { formatUsd } from '../../../../../../helpers/format';
 
 export const TIME_BUCKET: TimeBucketType[] = ['1h_1d', '1h_1w', '1d_1M', '1d_all'];
 
@@ -10,3 +11,23 @@ export const formatXAxis = (tickItem: number, timebucket: TimeBucketType) => {
   }
   return date.toLocaleDateString(undefined, { month: 'numeric', day: 'numeric' });
 };
+
+export function formatUnderlyingTick(value: number, minMax: [number, number]) {
+  const [, max] = minMax;
+
+  if (max >= 0.001) {
+    const decimals = max > 999 ? 0 : 3;
+
+    return value.toLocaleString('en-US', {
+      maximumFractionDigits: decimals,
+      minimumFractionDigits: decimals,
+    });
+  }
+
+  // if max is less than 0.001, we move to exponential notation
+  return value.toExponential(2);
+}
+
+export function formatUsdTick(value: number) {
+  return formatUsd(value);
+}
