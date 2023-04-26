e473505f716103c37515a48c1093d9d95bdcd9dd
erenjaegar77 <erenjaegar77@protonmail.com>
2023-04-25 23:26:52 -0300

    thorwback change on tokens script

--- scripts/common/tokens.ts
+++ scripts/common/tokens.ts
@@ -6,9 +6,14 @@ import {
 } from './config';
 import { getChainAddressBook } from '../../src/features/data/apis/addressbook';
 import { chainsByAppId } from './chains';
-import type { ChainEntity } from '../../src/features/data/entities/chain';
-import type { TokenEntity, TokenErc20, TokenNative } from '../../src/features/data/entities/token';
-import { isTokenErc20, isTokenNative } from '../../src/features/data/entities/token';
+import { ChainEntity } from '../../src/features/data/entities/chain';
+import {
+  isTokenErc20,
+  isTokenNative,
+  TokenEntity,
+  TokenErc20,
+  TokenNative,
+} from '../../src/features/data/entities/token';
 import { keyBy, mapValues } from 'lodash';
 
 /**
