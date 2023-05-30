e1c0b9f3abca9d04b6dcf67c5114c508e2bee442
erenjaegar77 <erenjaegar77@protonmail.com>
2023-05-30 12:36:20 -0300

    update safety score bars

--- src/components/SafetyScore/SafetyScore.tsx
+++ src/components/SafetyScore/SafetyScore.tsx
@@ -7,8 +7,6 @@ const useStyles = makeStyles(styles);
 
 export type SafetyScoreProps = {
   score: number;
-  whiteLabel?: boolean;
-  colorLabel?: boolean;
   size: 'sm' | 'md';
   align?: 'left' | 'right';
   className?: string;
@@ -16,28 +14,22 @@ export type SafetyScoreProps = {
 
 export const SafetyScore = memo<SafetyScoreProps>(function SafetyScore({
   score,
-  whiteLabel = false,
-  colorLabel = false,
   size = 'sm',
   align = 'left',
   className,
 }) {
   const classes = useStyles();
-  const scoreText = score === 0 ? '-' : score.toFixed(1);
 
   return (
     <div
       className={clsx(classes.container, className, {
         [classes.withSizeMedium]: size === 'md',
-        [classes.withWhiteLabel]: whiteLabel,
-        [classes.withColorLabel]: colorLabel,
         [classes.withScoreHigh]: score > 7.5,
-        [classes.withScoreMed]: score > 4 && score <= 7.5,
-        [classes.withScoreLow]: score > 0 && score <= 4,
+        [classes.withScoreMed]: score >= 6.4 && score <= 7.5,
+        [classes.withScoreLow]: score > 0 && score <= 6.4,
         [classes.withRightAlign]: align === 'right',
       })}
     >
-      <div className={classes.label}>{scoreText}</div>
       <div className={classes.barsContainer}>
         <div className={clsx(classes.bar, classes.sm)} />
         <div className={clsx(classes.bar, classes.md)} />
