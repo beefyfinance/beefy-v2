523f5ec586d096a8b36f32cff55c899aedaa2fae
erenjaegar77 <erenjaegar77@protonmail.com>
2023-07-21 10:52:00 -0300

    re enable conic migration and crvUSD USDC conic pools

--- src/config/migrators/ethereum.tsx
+++ src/config/migrators/ethereum.tsx
@@ -1,9 +1,9 @@
 import type { MigrationConfig } from '../../features/data/reducers/wallet/migration';
 
 export const migrators = [
-  // {
-  //   id: 'ethereum-conic',
-  //   name: 'Conic Finance',
-  //   icon: 'CNC',
-  // },
+  {
+    id: 'ethereum-conic',
+    name: 'Conic Finance',
+    icon: 'CNC',
+  },
 ] as const satisfies ReadonlyArray<MigrationConfig>;
