d92072fc3c19a8336b56b1420ee9085042a73d7a
erenjaegar77 <erenjaegar77@protonmail.com>
2023-07-25 14:52:06 -0300

    search platform filter

--- src/features/home/components/Filters/components/PlatformFilters/PlatformDropdownFilter.tsx
+++ src/features/home/components/Filters/components/PlatformFilters/PlatformDropdownFilter.tsx
@@ -1,15 +1,22 @@
+import type { ChangeEvent } from 'react';
 import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from '../../../../../../store';
 import { selectFilterPlatformId } from '../../../../../data/selectors/filtered-vaults';
-import type { ToggleButtonsProps } from '../../../../../../components/ToggleButtons';
 import { filteredVaultsActions } from '../../../../../data/reducers/filtered-vaults';
-import { LabeledSelect } from '../../../../../../components/LabeledSelect';
 import { selectFilterPlatforms } from '../../../../../data/selectors/platforms';
+import { Autocomplete } from '@material-ui/lab';
+import { TextField } from '@material-ui/core';
 
 export type PlatformDropdownFilterProps = {
   className?: string;
 };
+
+interface IListItem {
+  label: string;
+  value: string;
+}
+
 export const PlatformDropdownFilter = memo<PlatformDropdownFilterProps>(
   function PlatformDropdownFilter({ className }) {
     const { t } = useTranslation();
@@ -18,33 +25,42 @@ export const PlatformDropdownFilter = memo<PlatformDropdownFilterProps>(
     const placeholderAllKey = '__null';
     const otherKey = 'other';
     const platforms = useAppSelector(selectFilterPlatforms);
-    const options: Record<string, string> = useMemo(
-      () =>
-        Object.fromEntries([
-          [placeholderAllKey, t('Filter-DropdwnDflt')],
-          ...platforms.map(platform => [platform.id, platform.name]),
-          [otherKey, t('Filter-Other')],
-        ]),
-      [platforms, t]
-    );
+    const options = useMemo(() => {
+      const wrappedPlatforms = platforms.map(platform => {
+        return { label: platform.name, value: platform.id };
+      });
+      return [
+        { label: t('Filter-DropdwnDflt'), value: placeholderAllKey },
+        { label: t('Filter-Other'), value: otherKey },
+        ...wrappedPlatforms,
+      ];
+    }, [platforms, t]);
+
     const value = useAppSelector(selectFilterPlatformId);
-    const handleChange = useCallback<ToggleButtonsProps['onChange']>(
-      value => {
-        dispatch(filteredVaultsActions.setPlatformId(value === placeholderAllKey ? allKey : value));
+    const handleChange = useCallback(
+      (event: ChangeEvent, option: IListItem) => {
+        dispatch(
+          filteredVaultsActions.setPlatformId(
+            option.value === placeholderAllKey ? allKey : option.value
+          )
+        );
       },
-      [dispatch, placeholderAllKey, allKey]
+      [dispatch]
     );
 
     return (
-      <LabeledSelect
-        label={t('Filter-Platform')}
-        value={value === allKey ? placeholderAllKey : value}
-        options={options}
-        onChange={handleChange}
-        selectClass={className}
-        fullWidth={false}
-        sortOptions="label"
+      <Autocomplete
+        disablePortal={true}
         defaultValue={placeholderAllKey}
+        autoComplete={true}
+        getOptionLabel={(option: IListItem) => option.label}
+        closeIcon={null}
+        value={value}
+        onChange={handleChange}
+        className={className}
+        id="platforms-filter"
+        options={options}
+        renderInput={params => <TextField {...params} label="Platform" />}
       />
     );
   }
