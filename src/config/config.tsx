0d5bd5c7c0ad89065e772b0fa04744282bf21b42
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2023-08-07 17:16:05 +0100

    Fix Base Stable Tag

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -652,7 +652,7 @@ export const config: Record<ChainConfig['id'], Omit<ChainConfig, 'id'>> = {
     gas: {
       type: 'standard',
     },
-    stableCoins: ['USDbC, DAI, bsUSD, axlUSDC'],
+    stableCoins: ['USDbC', 'DAI', 'bsUSD', 'axlUSDC', 'MIM'],
   },
   aurora: {
     name: 'Aurora',
