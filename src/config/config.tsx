6325e58d574c04ada51cedfffbb0d79e7f5c8e11
erenjaegar77 <erenjaegar77@protonmail.com>
2023-09-01 15:39:09 -0300

    update kava rpc

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -540,7 +540,7 @@ export const config: Record<ChainConfig['id'], Omit<ChainConfig, 'id'>> = {
   kava: {
     name: 'Kava',
     chainId: 2222,
-    rpc: ['https://evm2.kava.io'],
+    rpc: ['https://kava-rpc.gateway.pokt.network'],
     explorerUrl: 'https://explorer.kava.io',
     multicallAddress: '0x13C6bCC2411861A31dcDC2f990ddbe2325482222',
     appMulticallContractAddress: '0x41D44B276904561Ac51855159516FD4cB2c90968',
@@ -553,7 +553,7 @@ export const config: Record<ChainConfig['id'], Omit<ChainConfig, 'id'>> = {
         symbol: 'KAVA',
         decimals: 18,
       },
-      rpcUrls: ['https://evm2.kava.io'],
+      rpcUrls: ['https://kava-rpc.gateway.pokt.network'],
       blockExplorerUrls: ['https://explorer.kava.io/'],
     },
     gas: {
