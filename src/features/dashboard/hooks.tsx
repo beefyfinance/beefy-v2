3b3cd7235170704d947726f2e40816a17d2f479e
erenjaegar77 <erenjaegar77@protonmail.com>
2023-10-04 17:18:30 -0300

    check if config is available before fetch user balance

--- src/features/dashboard/hooks.tsx
+++ src/features/dashboard/hooks.tsx
@@ -2,8 +2,10 @@ import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { initDashboardByAddress } from '../data/actions/wallet';
 import { selectWalletAddressIfKnown } from '../data/selectors/wallet';
+import { selectIsConfigAvailable } from '../data/selectors/data-loader';
 
 export function useInitDashboard(address: string) {
+  const isConfigAvailable = useAppSelector(selectIsConfigAvailable);
   const connectedAddress = useAppSelector(selectWalletAddressIfKnown);
   const addressBookLoaded = useAppSelector(
     state => state.ui.dataLoader.global.addressBook.alreadyLoadedOnce
@@ -11,8 +13,8 @@ export function useInitDashboard(address: string) {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (address && addressBookLoaded && address !== connectedAddress) {
+    if (isConfigAvailable && addressBookLoaded && address && address !== connectedAddress) {
       dispatch(initDashboardByAddress({ address }));
     }
-  }, [dispatch, address, connectedAddress, addressBookLoaded]);
+  }, [dispatch, address, connectedAddress, addressBookLoaded, isConfigAvailable]);
 }
