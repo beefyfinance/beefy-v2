d0e264b77ca8221c6f603527706f243810c2b55d
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-10-22 12:33:10 +0100

    Update addresses

--- src/config/beefy-bridge.ts
+++ src/config/beefy-bridge.ts
@@ -3,18 +3,17 @@ import BigNumber from 'bignumber.js';
 
 export const beefyBridgeConfig = {
   source: {
-    id: 'mooTest',
-    symbol: 'mooTest',
-    oracleId: 'mooFantomBIFI',
-    address: '0x3960f6c882d11B797bd3f574D77E339516813B5A',
+    id: 'mooBIFI',
+    symbol: 'mooBIFI',
+    oracleId: 'mooBIFI',
+    address: '0xBEEF8e0982874e0292E6C5751C5A4092b3e1BEEF',
     chainId: 'ethereum',
     decimals: 18,
-    description: 'Test BIFI description',
+    description: 'Receipt for BIFI deposited in the BIFI Maxi vault',
   },
   tokens: {
-    ethereum: '0x3B2faF664d6cCA5248CD7f44a6538A1fFdCcF7F0',
-    optimism: '0x665E21ce21B1e7c7401647c1fb740981b270b71d',
-    arbitrum: '0x508c6cF93e7D6793d7dB8b8B01ac6752A4275d75',
+    ethereum: '0xb1feA302f3B2E93FA04E46dCCE35F2Fc522d7bB9',
+    optimism: '0xc55E93C62874D8100dBd2DfE307EDc1036ad5434',
   },
   bridges: [
     {
@@ -23,7 +22,7 @@ export const beefyBridgeConfig = {
       explorerUrl: 'https://axelarscan.io/gmp/{{hash}}',
       chains: {
         ethereum: {
-          bridge: '0xaaa751957312589Cd21B2348f6B05b8b40691eF3',
+          bridge: '0xaaa6A279fC98b9bF94bD479C90D701417e361fc2',
           time: {
             outgoing: 20,
             incoming: 2,
@@ -35,24 +34,14 @@ export const beefyBridgeConfig = {
           },
         },
         optimism: {
-          bridge: '0xaaa751957312589Cd21B2348f6B05b8b40691eF3',
+          bridge: '0xaaa6A279fC98b9bF94bD479C90D701417e361fc2',
           time: {
             outgoing: 30,
             incoming: 2,
           },
           gasLimits: {
+            approve: new BigNumber('70000'),
             outgoing: new BigNumber('150000'), // ~140,163 before refunds
-            incoming: new BigNumber('170000'), // ~166,545 before refunds
-          },
-        },
-        arbitrum: {
-          bridge: '0xaaa751957312589Cd21B2348f6B05b8b40691eF3',
-          time: {
-            outgoing: 20,
-            incoming: 2,
-          },
-          gasLimits: {
-            outgoing: new BigNumber('150000'), // ~142,205 before refunds
             incoming: new BigNumber('170000'), // ~166,545 before refunds
           },
         },
@@ -64,7 +53,7 @@ export const beefyBridgeConfig = {
       explorerUrl: 'https://ccip.chain.link/tx/{{hash}}',
       chains: {
         ethereum: {
-          bridge: '0xcccd6EC6F4705292f07eE4a9a8F1F120963358c5',
+          bridge: '0xcccEa7Fe84272995664369334351Fe344E2732aE',
           time: {
             outgoing: 18,
             incoming: 5,
@@ -76,12 +65,13 @@ export const beefyBridgeConfig = {
           },
         },
         optimism: {
-          bridge: '0xcccd6EC6F4705292f07eE4a9a8F1F120963358c5',
+          bridge: '0xcccEa7Fe84272995664369334351Fe344E2732aE',
           time: {
             outgoing: 18,
             incoming: 3,
           },
           gasLimits: {
+            approve: new BigNumber('70000'),
             outgoing: new BigNumber('230000'), // ~216,481 before refunds
             incoming: new BigNumber('220000'), // ~211,298 before refunds
           },
@@ -94,7 +84,7 @@ export const beefyBridgeConfig = {
       explorerUrl: 'https://layerzeroscan.com/tx/{{hash}}',
       chains: {
         ethereum: {
-          bridge: '0xdddEedaBa09b08d052FaA67aCeCF36457252314f',
+          bridge: '0xdddaEc9c267dF24aD66Edc3B2cBe25dB86422051',
           time: {
             outgoing: 1,
             incoming: 4,
@@ -106,25 +96,15 @@ export const beefyBridgeConfig = {
           },
         },
         optimism: {
-          bridge: '0xdddEedaBa09b08d052FaA67aCeCF36457252314f',
+          bridge: '0xdddaEc9c267dF24aD66Edc3B2cBe25dB86422051',
           time: {
             outgoing: 1,
             incoming: 4,
           },
           gasLimits: {
+            approve: new BigNumber('70000'),
             outgoing: new BigNumber('280000'), // ~279,132 before refunds
             incoming: new BigNumber('270000'), // ~261,283 before refunds
-          },
-        },
-        arbitrum: {
-          bridge: '0xdddEedaBa09b08d052FaA67aCeCF36457252314f',
-          time: {
-            outgoing: 1,
-            incoming: 4,
-          },
-          gasLimits: {
-            outgoing: new BigNumber('290000'), // ~283,631 before refunds
-            incoming: new BigNumber('250000'), // ~239,127 before refunds
           },
         },
       },
@@ -134,25 +114,26 @@ export const beefyBridgeConfig = {
       title: 'Optimism',
       chains: {
         ethereum: {
-          bridge: '0xbbb6A6474729AEB3b2c83c4acCFf96711ff88589',
+          bridge: '0xbbb8971aEA2627fa2E1342bb5Bf952Ec521479f2',
           time: {
             outgoing: 2,
             incoming: 0,
           },
           gasLimits: {
             approve: new BigNumber('70000'),
-            outgoing: new BigNumber('1'),
-            incoming: new BigNumber('1'),
+            outgoing: new BigNumber('300000'), // ~296,305 before refunds
+            incoming: new BigNumber('140000'), // unknown TODO
           },
         },
         optimism: {
-          bridge: '0xbbb6A6474729AEB3b2c83c4acCFf96711ff88589',
+          bridge: '0xbbb8971aEA2627fa2E1342bb5Bf952Ec521479f2',
           time: {
             outgoing: 10080,
             incoming: 0,
           },
           gasLimits: {
-            outgoing: new BigNumber('1'),
+            approve: new BigNumber('70000'),
+            outgoing: new BigNumber('140000'), // ~130,947 before refunds
             incoming: new BigNumber('140000'), // ~134,500 before refunds
           },
         },
