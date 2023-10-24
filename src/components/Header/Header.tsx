ce48562b59fad00cac387ff698b8bf41b85b4d60
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-10-24 13:25:40 +0100

    Unhide BIFI price

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,7 +22,7 @@ import { DaoNavItems, ResourcesNavItems } from './list';
 import { ConnectionStatus } from './components/ConnectionStatus';
 import { DropNavItem } from './components/DropNavItem';
 import { MobileMenu } from './components/MobileMenu';
-// import { BifiPrice } from './components/BifiPrice';
+import { BifiPrice } from './components/BifiPrice';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { selectShouldInitProposals } from '../../features/data/selectors/data-loader';
 import { fetchActiveProposals } from '../../features/data/actions/proposal';
@@ -80,7 +80,7 @@ export const Header = memo(function Header() {
               <Hidden mdDown>
                 <NavItem title={'Header-BuyCrypto'} url="/onramp" Icon={BuyCryptoIcon} />
                 <NavItem title={'Header-BridgeBifi'} url="/bridge" Icon={BridgeIcon} />
-                {/* <BifiPrice />*/}
+                <BifiPrice />
               </Hidden>
               <ConnectionStatus />
               <Hidden lgUp>
