0b089f94738c5bc42bb09961b08d640918a0fe44
erenjaegar77 <erenjaegar77@protonmail.com>
2023-11-23 17:49:04 -0300

    fix wrong background for prices tooltip

--- src/components/Header/components/Prices/styles.tsx
+++ src/components/Header/components/Prices/styles.tsx
@@ -58,13 +58,13 @@ export const styles = (theme: Theme) => ({
     width: '24px',
   },
   tooltipContent: {
-    background: theme.palette.background.v2.button,
+    background: theme.palette.background.v2.contentPrimary,
     color: theme.palette.text.light,
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     padding: '16px',
   },
   tooltipArrow: {
-    color: theme.palette.background.v2.button,
+    color: theme.palette.background.v2.contentPrimary,
   },
   grid: {},
   tooltipTokens: {
