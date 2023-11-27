f75c8b17e8798ef8a6fc369596b86213032cd6c8
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-11-27 14:04:11 +0000

    add charts to gov pools + update featured vaults (#1866)
    * add charts to gov pools
    
    * always show
    
    * update feature vaults

--- src/features/vault/components/HistoricGraph/StatSwitcher/StatSwitcher.tsx
+++ src/features/vault/components/HistoricGraph/StatSwitcher/StatSwitcher.tsx
@@ -8,6 +8,7 @@ import { styles } from './styles';
 const useStyles = makeStyles(styles);
 
 export type StatSwitcherProps = {
+  type: 'standard' | 'gov';
   availableStats: ChartStat[];
   stat: ChartStat;
   onChange: (newStat: ChartStat) => void;
@@ -17,12 +18,15 @@ export const StatSwitcher = memo<StatSwitcherProps>(function StatSwitcher({
   availableStats,
   stat,
   onChange,
+  type,
 }) {
   const classes = useStyles();
   const { t } = useTranslation();
   const options: Record<string, string> = useMemo(() => {
-    return Object.fromEntries(availableStats.map(stat => [stat, t(`Graph-${stat}`)]));
-  }, [availableStats, t]);
+    return Object.fromEntries(
+      availableStats.map(stat => [stat, t([`Graph-${type}-${stat}`, `Graph-${stat}`])])
+    );
+  }, [availableStats, t, type]);
 
   return (
     <ToggleButtons value={stat} options={options} onChange={onChange} buttonsClass={classes.tabs} />
