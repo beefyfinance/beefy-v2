ecf6c76920735898201a7d4c7d385e52e2b0a9b4
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-12-16 18:53:05 +0000

    Fix: deposited vaults race condition (#1909)
    * Fix: deposited vaults race condition
    
    * comment
    
    * Don't let boost reward token override chain native token

--- src/features/data/reducers/wallet/balance.ts
+++ src/features/data/reducers/wallet/balance.ts
@@ -2,8 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import type BigNumber from 'bignumber.js';
 import type { Draft } from 'immer';
 import type { BeefyState } from '../../../../redux-types';
-import type { FetchAllBalanceFulfilledPayload } from '../../actions/balance';
-import { fetchAllBalanceAction, fetchBalanceAction } from '../../actions/balance';
+import {
+  fetchAllBalanceAction,
+  type FetchAllBalanceFulfilledPayload,
+  fetchBalanceAction,
+  recalculateDepositedVaultsAction,
+} from '../../actions/balance';
 import { initiateBoostForm } from '../../actions/boosts';
 import { reloadBalanceAndAllowanceAndGovRewardsAndBoostData } from '../../actions/tokens';
 import type {
@@ -15,17 +19,11 @@ import type { BoostEntity } from '../../entities/boost';
 import type { ChainEntity } from '../../entities/chain';
 import type { TokenEntity } from '../../entities/token';
 import type { VaultEntity } from '../../entities/vault';
-import { isGovVault, isStandardVault } from '../../entities/vault';
-import { selectAllVaultBoostIds, selectBoostById } from '../../selectors/boosts';
-import { selectAllVaultIds, selectVaultById } from '../../selectors/vaults';
+import { selectBoostById } from '../../selectors/boosts';
+import { selectVaultById } from '../../selectors/vaults';
 import { initiateMinterForm } from '../../actions/minters';
 import { selectMinterById } from '../../selectors/minters';
 import { BIG_ZERO } from '../../../../helpers/big-number';
-import {
-  selectBoostUserBalanceInToken,
-  selectGovVaultUserStakedBalanceInDepositToken,
-  selectUserBalanceOfToken,
-} from '../../selectors/balance';
 
 /**
  * State containing user balances state
@@ -116,11 +114,9 @@ export const balanceSlice = createSlice({
       addTokenBalanceToState(walletState, vault.chainId, balance.tokens);
       addGovVaultBalanceToState(walletState, balance.govVaults);
       addBoostBalanceToState(state, walletState, balance.boosts);
-      updateDepositedVaults(state, walletState, action.payload.walletAddress);
     });
 
     builder.addCase(initiateMinterForm.fulfilled, (sliceState, action) => {
-      const state = action.payload.state;
       if (!action.payload.walletAddress) {
         return;
       }
@@ -131,7 +127,6 @@ export const balanceSlice = createSlice({
       const balance = action.payload.balance;
 
       addTokenBalanceToState(walletState, minter.chainId, balance.tokens);
-      updateDepositedVaults(state, walletState, action.payload.walletAddress);
     });
 
     builder.addCase(
@@ -147,9 +142,13 @@ export const balanceSlice = createSlice({
         addTokenBalanceToState(walletState, chainId, balance.tokens);
         addGovVaultBalanceToState(walletState, balance.govVaults);
         addBoostBalanceToState(state, walletState, balance.boosts);
-        updateDepositedVaults(state, walletState, action.payload.walletAddress);
       }
     );
+
+    builder.addCase(recalculateDepositedVaultsAction.fulfilled, (sliceState, action) => {
+      const walletState = getWalletState(sliceState, action.payload.walletAddress.toLowerCase());
+      walletState.depositedVaultIds = action.payload.vaultIds;
+    });
   },
 });
 
@@ -168,74 +167,6 @@ function getWalletState(sliceState: Draft<BalanceState>, walletAddress: string) 
   return sliceState.byAddress[walletAddress];
 }
 
-function updateDepositedVaults(
-  state: BeefyState,
-  walletState: Draft<BalanceState['byAddress']['0xABC']>,
-  walletAddress: string
-) {
-  walletState.depositedVaultIds = calculateDepositedVaultsForAddress(state, walletAddress);
-}
-
-/**
- * Calculate all vaults that are deposited for a given address
- * For standard vaults, this means the user has a balance in the receipt token, or in the bridged receipt token, or in a boost
- * For gov vaults, this means the user has a gov vault balance
- */
-function calculateDepositedVaultsForAddress(state: BeefyState, walletAddress: string) {
-  const vaultIds = selectAllVaultIds(state);
-  const depositedIds: VaultEntity['id'][] = [];
-
-  for (const vaultId of vaultIds) {
-    const vault = selectVaultById(state, vaultId);
-
-    if (isStandardVault(vault)) {
-      // standard vaults via receipt tokens
-      let deposited = false;
-      const balance = selectUserBalanceOfToken(
-        state,
-        vault.chainId,
-        vault.earnContractAddress,
-        walletAddress
-      );
-      if (balance.gt(BIG_ZERO)) {
-        deposited = true;
-      }
-      // + boosts
-      if (!deposited) {
-        const boostIds = selectAllVaultBoostIds(state, vault.id);
-        for (const boostId of boostIds) {
-          const balance = selectBoostUserBalanceInToken(state, boostId, walletAddress);
-          if (balance.gt(BIG_ZERO)) {
-            deposited = true;
-            break;
-          }
-        }
-      }
-      // + bridged
-      if (!deposited && vault.bridged) {
-        for (const [chainId, bridgedAddress] of Object.entries(vault.bridged)) {
-          const balance = selectUserBalanceOfToken(state, chainId, bridgedAddress, walletAddress);
-          if (balance.gt(BIG_ZERO)) {
-            deposited = true;
-            break;
-          }
-        }
-      }
-      // add?
-      if (deposited) {
-        depositedIds.push(vault.id);
-      }
-    } else if (isGovVault(vault)) {
-      const balance = selectGovVaultUserStakedBalanceInDepositToken(state, vault.id, walletAddress);
-      if (balance.gt(BIG_ZERO)) {
-        depositedIds.push(vault.id);
-      }
-    }
-  }
-
-  return depositedIds;
-}
-
 function addBalancesToState(
   sliceState: Draft<BalanceState>,
   payload: FetchAllBalanceFulfilledPayload
@@ -249,7 +180,6 @@ function addBalancesToState(
   addTokenBalanceToState(walletState, chainId, balance.tokens);
   addBoostBalanceToState(state, walletState, balance.boosts);
   addGovVaultBalanceToState(walletState, balance.govVaults);
-  updateDepositedVaults(state, walletState, payload.walletAddress);
 }
 
 function addTokenBalanceToState(
