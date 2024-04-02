6ddf7ed7fc450f60b60d68fb70ee582c570de9bf
erenjaegar77 <erenjaegar77@protonmail.com>
2024-04-02 13:12:56 -0300

    remove april fools

--- src/features/pagenotfound/PageNotFound.tsx
+++ src/features/pagenotfound/PageNotFound.tsx
@@ -1,17 +1,15 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { styles } from './styles';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
 import image from '../../images/404image.svg';
-import aprilFools from '../../images/april-fools.svg';
 import { Button } from '../../components/Button';
 import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles(styles);
 
 export const PageNotFound = () => {
-  const { id } = useParams<{ id: string }>();
   const classes = useStyles();
   const { t } = useTranslation();
   const history = useHistory();
@@ -20,39 +18,16 @@ export const PageNotFound = () => {
     history.push(`/`);
   }, [history]);
 
-  const soylanaIds = [
-    'solana-sol-broken',
-    'wenona-wif-hat',
-    'turnona-bonk-sol',
-    'aprilfools-bome-sol',
-  ];
-
   return (
     <Container maxWidth="lg">
       <div className={classes.inner}>
-        {soylanaIds.includes(id) ? (
-          <>
-            <>
-              <img src={aprilFools} alt="404" className={classes.image} />
-              <div className={classes.textContainer}>
-                <div className={classes.text}>{t('Happy April Fools 🐮')}</div>
-                <Button variant="success" className={classes.button} onClick={handleOpen}>
-                  {t('View-All')}
-                </Button>
-              </div>
-            </>
-          </>
-        ) : (
-          <>
-            <img src={image} alt="404" className={classes.image} />
-            <div className={classes.textContainer}>
-              <div className={classes.text}>{t('Page-Not-Found')}</div>
-              <Button variant="success" className={classes.button} onClick={handleOpen}>
-                {t('View-All')}
-              </Button>
-            </div>
-          </>
-        )}
+        <img src={image} alt="404" className={classes.image} />
+        <div className={classes.textContainer}>
+          <div className={classes.text}>{t('Page-Not-Found')}</div>
+          <Button variant="success" className={classes.button} onClick={handleOpen}>
+            {t('View-All')}
+          </Button>
+        </div>
       </div>
     </Container>
   );
