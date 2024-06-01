9b029e53afba908136f4b5f00901c25751ef216c
chebiN <17909998+seguido@users.noreply.github.com>
2024-06-01 02:56:02 -0300

    zap in tx enabled

--- src/features/data/apis/transact/strategies/cowcentrated/CowcentratedStrategy.ts
+++ src/features/data/apis/transact/strategies/cowcentrated/CowcentratedStrategy.ts
@@ -1,19 +1,25 @@
 import type { CowcentratedStrategyOptions, IStrategy, ZapTransactHelpers } from '../IStrategy';
-import type {
-  CowcentratedDepositOption,
-  CowcentratedVaultDepositQuote,
-  DepositOption,
-  DepositQuote,
-  InputTokenAmount,
-  TokenAmount,
-  WithdrawOption,
-  WithdrawQuote,
-  ZapQuoteStep,
+import {
+  isZapQuoteStepSwap,
+  type CowcentratedDepositOption,
+  type CowcentratedVaultDepositQuote,
+  type DepositOption,
+  type DepositQuote,
+  type InputTokenAmount,
+  type TokenAmount,
+  type WithdrawOption,
+  type WithdrawQuote,
+  type ZapQuoteStep,
+  isZapQuoteStepBuild,
+  type ZapQuoteStepSwap,
+  isZapQuoteStepSwapAggregator,
+  type ZapQuoteStepSwapAggregator,
+  type ZapQuoteStepBuild,
 } from '../../transact-types';
 import type { Step } from '../../../../reducers/wallet/stepper';
 import type { Namespace, TFunction } from 'react-i18next';
 import type { CowcentratedVaultType } from '../../vaults/CowcentratedVaultType';
-import { isTokenEqual, isTokenErc20 } from '../../../../entities/token';
+import { isTokenEqual, isTokenErc20, isTokenNative } from '../../../../entities/token';
 import {
   createOptionId,
   createQuoteId,
@@ -21,14 +27,46 @@ import {
   onlyOneInput,
 } from '../../helpers/options';
 import { TransactMode } from '../../../../reducers/wallet/transact-types';
-import { BIG_ZERO, bigNumberToStringDeep, fromWei } from '../../../../../../helpers/big-number';
+import {
+  BIG_ZERO,
+  bigNumberToStringDeep,
+  fromWei,
+  toWei,
+  toWeiString,
+} from '../../../../../../helpers/big-number';
 import { selectChainById } from '../../../../selectors/chains';
 import { selectVaultStrategyAddress } from '../../../../selectors/vaults';
 import { BeefyCLMPool } from '../../../beefy/beefy-clm-pool';
 import { selectTokenPriceByAddress } from '../../../../selectors/tokens';
 import type { QuoteRequest } from '../../swap/ISwapProvider';
-import { first } from 'lodash-es';
+import { first, uniqBy } from 'lodash-es';
 import { calculatePriceImpact, highestFeeOrZero } from '../../helpers/quotes';
+import type { BeefyState, BeefyThunk } from '../../../../../../redux-types';
+import { selectTransactSlippage } from '../../../../selectors/transact';
+import type { ChainEntity } from '../../../../entities/chain';
+import type {
+  OrderInput,
+  OrderOutput,
+  UserlessZapRequest,
+  ZapStep,
+  ZapStepRequest,
+  ZapStepResponse,
+} from '../../zap/types';
+import { Balances } from '../../helpers/Balances';
+import { allTokensAreDistinct, pickTokens } from '../../helpers/tokens';
+import { fetchZapAggregatorSwap } from '../../zap/swap';
+import { NO_RELAY, getInsertIndex, getTokenAddress } from '../../helpers/zap';
+import abiCoder from 'web3-eth-abi';
+import { slipAllBy, slipBy } from '../../helpers/amounts';
+import { walletActions } from '../../../../actions/wallet-actions';
+import type BigNumber from 'bignumber.js';
+
+type ZapHelpers = {
+  chain: ChainEntity;
+  slippage: number;
+  state: BeefyState;
+  clmPool: BeefyCLMPool;
+};
 
 /**
  * This is just a wrapper around IVaultType to make it an IStrategy
@@ -97,8 +135,169 @@ export class CowcentratedStrategy<TOptions extends CowcentratedStrategyOptions>
     }
   }
 
-  async fetchDepositStep(quote: DepositQuote, t: TFunction<Namespace>): Promise<Step> {
-    return this.helpers.vaultType.fetchDepositStep(quote, t);
+  async fetchDepositStep(
+    quote: CowcentratedVaultDepositQuote,
+    t: TFunction<Namespace>
+  ): Promise<Step> {
+    const { vault } = this.helpers;
+    const vaultType = this.helpers.vaultType as CowcentratedVaultType;
+
+    console.log('fetchDepositStep::quote', quote);
+
+    const zapAction: BeefyThunk = async (dispatch, getState, extraArgument) => {
+      const state = getState();
+      const chain = selectChainById(state, vault.chainId);
+      const clmPool = new BeefyCLMPool(
+        vault.earnContractAddress,
+        selectVaultStrategyAddress(state, vault.id),
+        chain,
+        vaultType.depositTokens
+      );
+      const slippage = selectTransactSlippage(state);
+      const zapHelpers: ZapHelpers = { chain, slippage, state, clmPool };
+      const steps: ZapStep[] = [];
+      const minBalances = new Balances(quote.inputs);
+      const swapQuotes = quote.steps.filter(isZapQuoteStepSwap);
+      const buildQuote = quote.steps.find(isZapQuoteStepBuild);
+
+      if (!buildQuote || swapQuotes.length > 2) {
+        throw new Error('Invalid quote');
+      }
+
+      // Swaps
+      const insertBalance = allTokensAreDistinct(
+        swapQuotes
+          .map(quoteStep => quoteStep.fromToken)
+          .concat(buildQuote.inputs.map(({ token }) => token))
+      );
+      const swapZaps = await Promise.all(
+        swapQuotes.map(quoteStep => this.fetchZapSwap(quoteStep, zapHelpers, insertBalance))
+      );
+      swapZaps.forEach(swap => {
+        // add step to order
+        swap.zaps.forEach(step => steps.push(step));
+        // track the minimum balances for use in further steps
+        minBalances.subtractMany(swap.inputs);
+        minBalances.addMany(swap.minOutputs);
+      });
+
+      // Build LP
+      const buildZap = await this.fetchZapBuildCLM(
+        buildQuote,
+        buildQuote.inputs.map(({ token }) => ({
+          token,
+          amount: minBalances.get(token), // we have to pass min expected in case swaps slipped
+        })),
+        zapHelpers
+      );
+      buildZap.zaps.forEach(step => steps.push(step));
+      minBalances.subtractMany(buildZap.inputs);
+      minBalances.addMany(buildZap.minOutputs);
+
+      // Build order
+      const inputs: OrderInput[] = quote.inputs.map(input => ({
+        token: getTokenAddress(input.token),
+        amount: toWeiString(input.amount, input.token.decimals),
+      }));
+
+      const requiredOutputs: OrderOutput[] = buildZap.outputs.map(output => ({
+        token: getTokenAddress(output.token),
+        minOutputAmount: toWeiString(
+          slipBy(output.amount, slippage, output.token.decimals),
+          output.token.decimals
+        ),
+      }));
+
+      // We need to list all inputs, and mid-route outputs, as outputs so dust gets returned
+      const dustOutputs: OrderOutput[] = pickTokens(
+        quote.outputs,
+        quote.inputs,
+        quote.returned
+      ).map(token => ({
+        token: getTokenAddress(token),
+        minOutputAmount: '0',
+      }));
+
+      swapQuotes.forEach(quoteStep => {
+        dustOutputs.push({
+          token: getTokenAddress(quoteStep.fromToken),
+          minOutputAmount: '0',
+        });
+        dustOutputs.push({
+          token: getTokenAddress(quoteStep.toToken),
+          minOutputAmount: '0',
+        });
+      });
+      // dustOutputs.push({
+      //   token: getTokenAddress(buildQuote.outputToken),
+      //   minOutputAmount: '0',
+      // });
+
+      // @dev uniqBy: first occurrence of each element is kept.
+      const outputs = uniqBy(requiredOutputs.concat(dustOutputs), output => output.token);
+
+      // Perform TX
+      const zapRequest: UserlessZapRequest = {
+        order: {
+          inputs,
+          outputs,
+          relay: NO_RELAY,
+        },
+        steps,
+      };
+
+      const expectedTokens = buildZap.outputs.map(output => output.token);
+      const walletAction = walletActions.zapExecuteOrder(
+        quote.option.vaultId,
+        zapRequest,
+        expectedTokens
+      );
+
+      return walletAction(dispatch, getState, extraArgument);
+    };
+
+    return {
+      step: 'zap-in',
+      message: t('Vault-TxnConfirm', { type: t('Deposit-noun') }),
+      action: zapAction,
+      pending: false,
+      extraInfo: { zap: true, vaultId: quote.option.vaultId },
+    };
+  }
+
+  protected async fetchZapSwap(
+    quoteStep: ZapQuoteStepSwap,
+    zapHelpers: ZapHelpers,
+    insertBalance: boolean
+  ): Promise<ZapStepResponse> {
+    if (isZapQuoteStepSwapAggregator(quoteStep)) {
+      return this.fetchZapSwapAggregator(quoteStep, zapHelpers, insertBalance);
+    } else {
+      throw new Error('Unknown zap quote swap step type');
+    }
+  }
+
+  protected async fetchZapSwapAggregator(
+    quoteStep: ZapQuoteStepSwapAggregator,
+    zapHelpers: ZapHelpers,
+    insertBalance: boolean
+  ): Promise<ZapStepResponse> {
+    const { swapAggregator, zap } = this.helpers;
+    const { slippage, state } = zapHelpers;
+
+    return await fetchZapAggregatorSwap(
+      {
+        quote: quoteStep.quote,
+        inputs: [{ token: quoteStep.fromToken, amount: quoteStep.fromAmount }],
+        outputs: [{ token: quoteStep.toToken, amount: quoteStep.toAmount }],
+        maxSlippage: slippage,
+        zapRouter: zap.router,
+        providerId: quoteStep.providerId,
+        insertBalance,
+      },
+      swapAggregator,
+      state
+    );
   }
 
   async fetchWithdrawOptions(): Promise<WithdrawOption[]> {
@@ -295,6 +494,118 @@ export class CowcentratedStrategy<TOptions extends CowcentratedStrategyOptions>
       amountsUsed: [],
       amountsReturned: [],
       isCalm,
+    };
+  }
+
+  protected async fetchZapBuildCLM(
+    quoteStep: ZapQuoteStepBuild,
+    minInputs: TokenAmount[],
+    zapHelpers: ZapHelpers
+  ): Promise<ZapStepResponse> {
+    const { zap } = this.helpers;
+    const { slippage, clmPool } = zapHelpers;
+
+    const {
+      liquidity: liquidityWei,
+      amount0,
+      amount1,
+    } = await clmPool.previewDeposit(minInputs[0].amount, minInputs[1].amount);
+
+    const liquidity = fromWei(liquidityWei, quoteStep.outputToken.decimals);
+    const addAmounts = [amount0, amount1].map((amount, i) => ({
+      token: minInputs[i].token,
+      amount: fromWei(amount, minInputs[i].token.decimals),
+    }));
+
+    return await this.getZapBuildCLM({
+      inputs: addAmounts,
+      outputs: [{ token: quoteStep.outputToken, amount: liquidity }],
+      maxSlippage: slippage,
+      zapRouter: zap.router,
+      insertBalance: true,
+    });
+  }
+
+  protected async getZapBuildCLM(request: ZapStepRequest): Promise<ZapStepResponse> {
+    const clmVault = this.helpers.vaultType as CowcentratedVaultType;
+    const { inputs, outputs, maxSlippage, insertBalance } = request;
+
+    if (inputs.length !== 2) {
+      throw new Error('Invalid inputs');
+    }
+
+    for (const input of inputs) {
+      if (isTokenNative(input.token)) throw new Error('Invalid token');
+      if (!clmVault.depositTokens.find(token => isTokenEqual(token, input.token))) {
+        throw new Error('Invalid token');
+      }
+    }
+
+    return {
+      inputs,
+      outputs,
+      minOutputs: slipAllBy(outputs, maxSlippage),
+      returned: [],
+      zaps: [
+        this.buildZapbuildCLMTx(
+          clmVault.shareToken.address,
+          toWei(inputs[0].amount, inputs[0].token.decimals),
+          toWei(inputs[1].amount, inputs[1].token.decimals),
+          toWei(outputs[0].amount, outputs[0].token.decimals),
+          inputs[0].token.address,
+          inputs[1].token.address,
+          insertBalance
+        ),
+      ],
+    };
+  }
+
+  protected buildZapbuildCLMTx(
+    clmAddress: string,
+    amountA: BigNumber,
+    amountB: BigNumber,
+    liquidity: BigNumber,
+    tokenA: string,
+    tokenB: string,
+    insertBalance: boolean
+  ): ZapStep {
+    return {
+      target: clmAddress,
+      value: '0',
+      data: abiCoder.encodeFunctionCall(
+        {
+          type: 'function',
+          name: 'deposit',
+          constant: false,
+          payable: false,
+          inputs: [
+            {
+              name: '_amount0',
+              type: 'uint256',
+            },
+            {
+              name: '_amount1',
+              type: 'uint256',
+            },
+            {
+              name: '_minShares',
+              type: 'uint256',
+            },
+          ],
+          outputs: [],
+        },
+        [amountA.toString(10), amountB.toString(10), liquidity.toString()]
+      ),
+      tokens: [
+        {
+          token: tokenA,
+          index: insertBalance ? getInsertIndex(0) : -1, // amountADesired
+        },
+        {
+          token: tokenB,
+          index: insertBalance ? getInsertIndex(1) : -1, // amountBDesired
+        },
+      ],
     };
   }
 }
