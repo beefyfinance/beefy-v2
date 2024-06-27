d74394a266f6da2e297885b67d19ed837bd285d0
Eren Jaegar <88328748+erenjaegar77@users.noreply.github.com>
2024-06-27 07:28:25 -0300

    Update/Filters options (#2210)
    * update filter options
    
    * remove featured vaults
    
    * remove 'vaults
    ' from filters texts
    
    * filters funtionability
    
    * apply multi category filters
    
    * update methods and cleanup
    
    * don't use selectors when we already have vault entity
    
    ---------
    
    Co-authored-by: EREN <eren@ERENs-Laptop.local>
    Co-authored-by: ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>

--- src/features/home/components/Filters/components/VaultCategoryFilters/VaultCategoryDropdownFilter.tsx
+++ src/features/home/components/Filters/components/VaultCategoryFilters/VaultCategoryDropdownFilter.tsx
@@ -2,37 +2,15 @@ import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from '../../../../../../store';
 import { selectFilterVaultCategory } from '../../../../../data/selectors/filtered-vaults';
-import { type ToggleButtonsProps } from '../../../../../../components/ToggleButtons';
 import type { FilteredVaultsState } from '../../../../../data/reducers/filtered-vaults';
 import { filteredVaultsActions } from '../../../../../data/reducers/filtered-vaults';
-import {
-  DropdownItemLabel,
-  type DropdownItemLabelProps,
-  LabeledSelect,
-} from '../../../../../../components/LabeledSelect';
+
 import { CATEGORY_OPTIONS } from './category-options';
-import { makeStyles } from '@material-ui/core';
-import { styles } from './styles';
 
-const useStyles = makeStyles(styles);
-
-const CategoryDropdownLabel = memo<DropdownItemLabelProps>(function CategoryDropdownLabel(props) {
-  const classes = useStyles();
-  const { value, label: originalLabel } = props;
-  const label = useMemo(() => {
-    const option = CATEGORY_OPTIONS[value];
-    if (option && option.highlight) {
-      return (
-        <div className={classes.holder}>
-          {originalLabel} <span className={classes.highlight}>{option.highlight}</span>
-        </div>
-      );
-    }
-    return originalLabel;
-  }, [value, originalLabel, classes]);
-
-  return <DropdownItemLabel {...props} label={label} />;
-});
+import {
+  LabeledMultiSelect,
+  type LabeledMultiSelectProps,
+} from '../../../../../../components/LabeledMultiSelect';
 
 export type VaultCategoryDropdownFilterProps = {
   className?: string;
@@ -49,24 +27,27 @@ export const VaultCategoryDropdownFilter = memo<VaultCategoryDropdownFilterProps
       [t]
     );
     const value = useAppSelector(selectFilterVaultCategory);
-    const handleChange = useCallback<ToggleButtonsProps['onChange']>(
-      value => {
+    const handleChange = useCallback<LabeledMultiSelectProps['onChange']>(
+      selected => {
         dispatch(
-          filteredVaultsActions.setVaultCategory(value as FilteredVaultsState['vaultCategory'])
+          filteredVaultsActions.setVaultCategory(
+            selected.length === Object.values(options).length
+              ? []
+              : (selected as FilteredVaultsState['vaultCategory'])
+          )
         );
       },
-      [dispatch]
+      [dispatch, options]
     );
 
     return (
-      <LabeledSelect
+      <LabeledMultiSelect
         label={t('Filter-Category')}
         value={value}
         options={options}
         onChange={handleChange}
         selectClass={className}
         fullWidth={true}
-        DropdownItemLabelComponent={CategoryDropdownLabel}
       />
     );
   }
