98ae0a05a17f6e9a9bc3be8cadb13d5e9b67b155
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-07-03 10:20:45 +0100

    don't use theme before theme provider

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -9,16 +9,20 @@ const DefaultFallbackComponent: FC<ErrorBoundaryHasErrorState> = import.meta.env
   : DefaultFallback;
 
 function isReactDevTools(): boolean {
-  type ErrorFnWithDevTools = typeof console.error & {
-    __REACT_DEVTOOLS_STRICT_MODE_ORIGINAL_METHOD__?: unknown | undefined;
-    __REACT_DEVTOOLS_ORIGINAL_METHOD__?: unknown | undefined;
-  };
-  const errorFn = console.error as ErrorFnWithDevTools;
+  try {
+    type ErrorFnWithDevTools = typeof console.error & {
+      __REACT_DEVTOOLS_STRICT_MODE_ORIGINAL_METHOD__?: unknown | undefined;
+      __REACT_DEVTOOLS_ORIGINAL_METHOD__?: unknown | undefined;
+    };
+    const errorFn = console.error as ErrorFnWithDevTools;
 
-  return (
-    !!errorFn.__REACT_DEVTOOLS_ORIGINAL_METHOD__ ||
-    !!errorFn.__REACT_DEVTOOLS_STRICT_MODE_ORIGINAL_METHOD__
-  );
+    return (
+      !!errorFn.__REACT_DEVTOOLS_ORIGINAL_METHOD__ ||
+      !!errorFn.__REACT_DEVTOOLS_STRICT_MODE_ORIGINAL_METHOD__
+    );
+  } catch {
+    return false;
+  }
 }
 
 export class ErrorBoundary extends PureComponent<ErrorBoundaryProps, ErrorBoundaryState> {
@@ -28,40 +32,57 @@ export class ErrorBoundary extends PureComponent<ErrorBoundaryProps, ErrorBounda
   }
 
   static getDerivedStateFromError(e: unknown): ErrorBoundaryState {
-    const error = miniSerializeError(e);
+    if (import.meta.env.DEV) {
+      const error = miniSerializeError(e);
 
-    if (e instanceof Error && e.stack) {
-      error.stack = e.stack
-        .split('\n')
-        .filter(line => !line.includes('/node_modules/') && line.includes(window.location.origin))
-        .map(line => line.replace(window.location.origin, '').trim())
-        .join('\n');
+      if (e instanceof Error && e.stack) {
+        error.stack = e.stack
+          .split('\n')
+          .filter(line => !line.includes('/node_modules/') && line.includes(window.location.origin))
+          .map(line => line.replace(window.location.origin, '').trim())
+          .join('\n');
+      }
+
+      return { hasError: true, error };
     }
 
-    return { hasError: true, error };
+    return {
+      hasError: true,
+      error: {
+        message:
+          (!!e &&
+            typeof e === 'object' &&
+            'message' in e &&
+            typeof e.message === 'string' &&
+            e.message) ||
+          'An unexpected error occurred',
+      },
+    };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    if (isReactDevTools()) return;
+    if (import.meta.env.DEV) {
+      if (isReactDevTools()) return;
 
-    const title = this.state.hasError
-      ? this.state.error.message || this.state.error.name || error.name
-      : error.name;
+      const title = this.state.hasError
+        ? this.state.error.message || this.state.error.name || error.name
+        : error.name;
 
-    console.group(`%c❌❌❌ ErrorBoundary caught an error ⤵️⤵️⤵️`, `background: #4a3535;`);
-    console.log(title);
-    if (error.stack) {
-      console.groupCollapsed('%cErrorStack ->', `background: #4a3535;`);
-      console.log(error.stack);
+      console.group(`%c❌❌❌ ErrorBoundary caught an error ⤵️⤵️⤵️`, `background: #4a3535;`);
+      console.log(title);
+      if (error.stack) {
+        console.groupCollapsed('%cErrorStack ->', `background: #4a3535;`);
+        console.log(error.stack);
+        console.groupEnd();
+      }
+      if (errorInfo.componentStack) {
+        console.groupCollapsed('%cComponentStack ->', `background: #4a3535;`);
+        console.log(errorInfo.componentStack);
+        console.groupEnd();
+      }
+      console.log(`%c❌❌❌ ErrorBoundary caught an error ⤴️⤴️⤴️`, `background: #4a3535;`);
       console.groupEnd();
     }
-    if (errorInfo.componentStack) {
-      console.groupCollapsed('%cComponentStack ->', `background: #4a3535;`);
-      console.log(errorInfo.componentStack);
-      console.groupEnd();
-    }
-    console.log(`%c❌❌❌ ErrorBoundary caught an error ⤴️⤴️⤴️`, `background: #4a3535;`);
-    console.groupEnd();
   }
 
   render() {
