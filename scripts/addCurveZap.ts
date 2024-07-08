17e6dbf9a0c28e8eae5f9f331d423f17eee95155
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-07-08 10:32:12 +0100

    fix addCurveZap types (#2264)

--- scripts/addCurveZap.ts
+++ scripts/addCurveZap.ts
@@ -15,9 +15,9 @@ import { MultiCall, ShapeWithLabel } from 'eth-multicall';
 import { mkdir } from 'node:fs/promises';
 import * as path from 'node:path';
 import { isNonEmptyArray, NonEmptyArray, sleep } from './common/utils';
+import type { CurveStrategyConfig } from '../src/features/data/apis/transact/strategies/strategy-configs';
 import type { CurveMethodTypes } from '../src/features/data/apis/transact/strategies/curve/types';
 import { getAddress } from 'viem';
-import { ZapStrategyConfig } from '../src/features/data/apis/transact/strategies/strategy-configs';
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const EEEE_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
@@ -1061,17 +1061,19 @@ export async function discoverCurveZap(args: RunArgs) {
 
   const methods = await poolToMethods(pool);
   if (methods.length) {
-    return {
+    const zap: CurveStrategyConfig = {
       strategyId: 'curve',
       poolAddress: pool.address,
       methods,
     };
+
+    return zap;
   } else {
     throw new Error(`No zap methods found for pool`);
   }
 }
 
-async function saveZap(chainId: string, vaultId: string, zap: any) {
+async function saveZap(chainId: string, vaultId: string, zap: CurveStrategyConfig) {
   const path = `./src/config/vault/${addressBookToAppId(chainId)}.json`;
   const vaults = await loadJson<VaultConfig[]>(path);
   let found = false;
