8c3f02b9ea34cdc2bffd79e490ce91de5c343682
EREN <eren@ERENs-Laptop.local>
2024-11-13 21:21:50 +0700

    update types

--- src/helpers/getConfigSrc.tsx
+++ src/helpers/getConfigSrc.tsx
@@ -2,20 +2,18 @@ import type { MinterConfig } from '../features/data/apis/config-types';
 import type { ChainEntity } from '../features/data/entities/chain';
 import type { MigrationConfig } from '../features/data/reducers/wallet/migration';
 
-const mintersPathToImportFn = import.meta.glob<{ minters: MinterConfig[] }>(
-  '../config/minters/*.tsx',
-  { import: 'minters' }
-);
+const mintersPathToImportFn = import.meta.glob<MinterConfig[]>('../config/minters/*.tsx', {
+  import: 'minters',
+});
 
 export async function getMinterConfigSrc(chainId: ChainEntity['id']) {
   const importFn = mintersPathToImportFn[`../config/minters/${chainId}.tsx`];
   return importFn ? await importFn() : [];
 }
 
-const migratorsPathToImportFn = import.meta.glob<{ migrators: MigrationConfig[] }>(
-  '../config/migrators/*.tsx',
-  { import: 'migrators' }
-);
+const migratorsPathToImportFn = import.meta.glob<MigrationConfig[]>('../config/migrators/*.tsx', {
+  import: 'migrators',
+});
 
 export async function getMigratorConfigSrc(chainId: ChainEntity['id']) {
   const importFn = migratorsPathToImportFn[`../config/migrators/${chainId}.tsx`];
