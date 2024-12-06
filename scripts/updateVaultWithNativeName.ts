bf6128011b30eab7051cbc30a8a5d39a3afa726b
EREN <eren@ERENs-Laptop.local>
2024-12-06 13:55:45 -0300

    update all native in names to wnative for active chains and vaults

--- scripts/updateVaultWithNativeName.ts
+++ scripts/updateVaultWithNativeName.ts
@@ -2,33 +2,36 @@
 import { promises as fs } from 'fs';
 import { config } from '../src/config/config';
 import { saveJson } from './common/files';
+import { ChainConfig } from '../src/features/data/apis/config-types';
 
 const vaultsDir = './src/config/vault/';
 
-async function updateNames() {
-  for (const [chainKey, chainInfo] of Object.entries(config)) {
-    const vaultsFile = `${vaultsDir}${chainKey}.json`;
-    const vaults = JSON.parse(await fs.readFile(vaultsFile, 'utf8'));
+async function pause() {
+  for (const [chainKey, chainInfo] of Object.entries(config) as unknown as [
+    string,
+    ChainConfig
+  ][]) {
+    if (chainKey === 'base') {
+      //skip eol chains
+      const vaultsFile = `${vaultsDir}${chainKey}.json`;
+      const vaults = JSON.parse(await fs.readFile(vaultsFile, 'utf8'));
 
-    vaults.forEach(v => {
-      // Create regular expressions for exact matches of native and wrapped tokens
-      const nativeRegex = new RegExp(`\\b${chainInfo.native.symbol}([/\s$])`, 'gmi'); // Match the native token as a whole word
-      // const nativeRegex = new RegExp(`/\b${chainInfo.native.symbol}([-/\s$])/gmi`)
-      const wrappedRegex = new RegExp(`\\b${chainInfo.native.oracleId}\\b`, 'i'); // Match the wrapped token as a whole word
-      // const wrappedRegex = new RegExp(`${chainInfo.native.oracleId}$1`); // Match the wrapped token as a whole word
+      vaults.forEach(v => {
+        if (
+          v.name.includes(`-${chainInfo.native.symbol} `) &&
+          !v.name.includes(chainInfo.native.oracleId) &&
+          v.status === 'active'
+        ) {
+          v.name = v.name.replace(chainInfo.native.symbol, chainInfo.native.oracleId);
+        }
+      });
 
-      // Check if the vault name includes the native token and not the wrapped token
-      if (nativeRegex.test(v.name) && !wrappedRegex.test(v.name)) {
-        // Replace the native token with the wrapped token, handling hyphenated pairs
-        v.name = v.name.replace(nativeRegex, chainInfo.native.oracleId);
-      }
-    });
-
-    await saveJson(vaultsFile, vaults, 'prettier');
+      await saveJson(vaultsFile, vaults, 'prettier');
+    }
   }
 }
 
-updateNames().catch(err => {
+pause().catch(err => {
   console.error(err);
   process.exit(-1);
 });
