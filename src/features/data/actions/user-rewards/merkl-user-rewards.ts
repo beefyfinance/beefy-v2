1a57f8b5e12c7daac8ac9a635f3db9b96fe08f14
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2025-01-25 17:45:47 +0000

    Add merkl distributor address for sonic

--- src/features/data/actions/user-rewards/merkl-user-rewards.ts
+++ src/features/data/actions/user-rewards/merkl-user-rewards.ts
@@ -43,6 +43,7 @@ export const MERKL_SUPPORTED_CHAINS: Partial<Record<ChainEntity['id'], Address>>
   celo: '0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae',
   sei: '0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae',
   rootstock: '0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae',
+  sonic: '0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae',
 };
 
 function parseReasonId(
