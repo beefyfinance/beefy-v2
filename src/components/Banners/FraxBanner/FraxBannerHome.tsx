5f2b8ac8d8564b7175b2b1babeed5104d9311cbd
erenjaegar77 <erenjaegar77@protonmail.com>
2025-07-16 17:55:36 -0300

    remove frax banner

--- src/components/Banners/FraxBanner/FraxBannerHome.tsx
+++
@@ -1,9 +0,0 @@
-import { memo } from 'react';
-import { selectHasUserDepositedOnChain } from '../../../features/data/selectors/balance.ts';
-import { useAppSelector } from '../../../features/data/store/hooks.ts';
-import { FraxBanner } from './FraxBanner.tsx';
-
-export const FraxBannerHome = memo(function FraxBannerHome() {
-  const hasDeposits = useAppSelector(state => selectHasUserDepositedOnChain(state, 'fraxtal'));
-  return hasDeposits ? <FraxBanner /> : null;
-});
