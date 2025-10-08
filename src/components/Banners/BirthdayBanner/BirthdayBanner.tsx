313a5db07195c1f627fc55c3f559b29b8ab577f9
Pablo <78693916+pablobeefy@users.noreply.github.com>
2025-10-08 16:22:54 +0300

    Update BirthdayBanner.tsx

--- src/components/Banners/BirthdayBanner/BirthdayBanner.tsx
+++ src/components/Banners/BirthdayBanner/BirthdayBanner.tsx
@@ -7,7 +7,7 @@ export const BirthdayBanner = memo(function BirthdayBanner() {
     <DismissibleBanner
       id="birthday-banner"
       icon={<img src={birthdayIcon} alt="" width={24} height={24} />}
-      text="Happy Birthday Beefy!"
+      text="Celebrate Beefy’s 5th birthday! Discover our story and share your #IUsedBeefy moment for a chance to join the Hall of Fame."
     />
   );
 });
