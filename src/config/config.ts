e17dc356cdd59b2944586e35357ffceaafc605c9
RM <roman-monk@users.noreply.github.com>
2025-10-14 13:26:00 +0800

    Add PlasmaUSD

--- src/config/config.ts
+++ src/config/config.ts
@@ -868,7 +868,7 @@ export const config = {
       baseSafetyMargin: 0.2,
       priorityMinimum: '10000000', // 0.01 gwei
     },
-    stableCoins: ['USDT0', 'USDe', 'sUSDe'],
+    stableCoins: ['USDT0', 'USDe', 'sUSDe', 'PlasmaUSD'],
   },
   aurora: {
     name: 'Aurora',
