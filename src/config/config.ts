2add800afd2b7f9516a44c86a3ccffbc1c1b33f5
RM <roman-monk@users.noreply.github.com>
2025-10-23 14:24:32 +0800

    Add pendle-plasma-splusd-29jan26

--- src/config/config.ts
+++ src/config/config.ts
@@ -873,7 +873,7 @@ export const config = {
       baseSafetyMargin: 0.2,
       priorityMinimum: '10000000', // 0.01 gwei
     },
-    stableCoins: ['USDT0', 'USDe', 'sUSDe', 'PlasmaUSD', 'msUSD'],
+    stableCoins: ['USDT0', 'USDe', 'sUSDe', 'PlasmaUSD', 'msUSD', 'splUSD'],
   },
   aurora: {
     name: 'Aurora',
