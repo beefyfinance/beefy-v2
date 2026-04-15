cf5b5ab7e382ebca2086772b04caf412a5472a64
erenjaegar77 <erenjaegar77@protonmail.com>
2026-04-15 15:35:34 -0300

    update: consume largeHolders risk from ab

--- src/features/data/selectors/risks.ts
+++ src/features/data/selectors/risks.ts
@@ -9,8 +9,15 @@ import type { Token } from '@beefyfinance/blockchain-addressbook';
 
 export type RiskKeys = Exclude<keyof VaultEntity['risks'], 'updatedAt'>;
 export type RiskChange = { key: RiskKeys; value: boolean };
-type TokenTag = Exclude<Token['tags'], undefined>[number];
-type TokenTagToRiskMap = { [K in TokenTag]?: RiskChange };
+export type TokenTagRiskKeys = 'largeHolders';
+export type TokenRiskKeys = RiskKeys | TokenTagRiskKeys;
+export type TokenRiskChange = { key: TokenRiskKeys; value: boolean };
+type TokenTags = Exclude<Token['tags'], undefined>[number];
+export type TokenOnlyTags = Extract<
+  TokenTags,
+  'LARGE_HOLDERS' | 'NO_TIMELOCK' | 'SYNTHETIC' | 'CURATED'
+>;
+type TokenTagToRiskMap = { [K in TokenTags]?: TokenRiskChange };
 
 export const platformRiskMap: Record<string, RiskChange> = {
   NO_TIMELOCK: { key: 'notTimelocked', value: true },
@@ -18,10 +25,11 @@ export const platformRiskMap: Record<string, RiskChange> = {
   NOT_BATTLE_TESTED: { key: 'notBattleTested', value: true },
 };
 
-export const tokenTagToRiskMap: Record<string, RiskChange> = {
+export const tokenTagToRiskMap: Record<string, TokenRiskChange> = {
   NO_TIMELOCK: { key: 'notTimelocked', value: true },
   SYNTHETIC: { key: 'synthAsset', value: true },
   CURATED: { key: 'curated', value: true },
+  LARGE_HOLDERS: { key: 'largeHolders', value: true },
 } satisfies TokenTagToRiskMap;
 
 const selectVaultRisks = createSelector(
@@ -29,29 +37,22 @@ const selectVaultRisks = createSelector(
   selectVaultPlatformOrUndefined,
   selectVaultAssetTokensOrUndefined,
   (vault, platform, tokens) => {
-    if (
-      (!platform || !platform.risks || platform.risks.length === 0) &&
-      (!tokens || tokens.length === 0)
-    ) {
-      return vault.risks;
-    }
-
+    const risks: VaultEntity['risks'] & Record<TokenTagRiskKeys, boolean> = {
+      ...vault.risks,
+      largeHolders: false,
+    };
     const platformRisks =
       platform?.risks
         .map(k => platformRiskMap[k])
-        .filter(change => change !== undefined && vault.risks[change.key] !== change.value) || [];
+        .filter(change => change !== undefined && risks[change.key] !== change.value) || [];
     const tokenRisks =
       tokens
         ?.filter(isDefined)
         .filter(isTokenErc20)
         .flatMap(token => token.tags ?? [])
         .map(k => tokenTagToRiskMap[k])
-        .filter(change => change !== undefined && vault.risks[change.key] !== change.value) || [];
-    if (platformRisks.length === 0 && tokenRisks.length === 0) {
-      return vault.risks;
-    }
+        .filter(change => change !== undefined && risks[change.key] !== change.value) || [];
 
-    const risks = { ...vault.risks };
     for (const change of platformRisks) {
       risks[change.key] = change.value;
     }
