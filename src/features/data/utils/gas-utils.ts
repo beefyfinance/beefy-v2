93ca2c552aa9c099fca1051b56b77fc9dfd1f257
jackgale.eth <jack.gale@icloud.com>
2026-08-19 18:59:38 +0100

    Revert gas changes

--- src/features/data/utils/gas-utils.ts
+++ src/features/data/utils/gas-utils.ts
@@ -1,19 +1,8 @@
 import type { ChainEntity } from '../entities/chain.ts';
 import { getGasPricer } from '../apis/instances.ts';
 
-type GasLimitType = 'approval' | 'other';
-
-// wallet-side gas estimation is unreliable on base for b20 tokens, we can remove once fixed
-const GAS_LIMIT_OVERRIDES: Partial<Record<ChainEntity['id'], Record<GasLimitType, bigint>>> = {
-  base: {
-    approval: 1_000_000n,
-    other: 9_000_000n,
-  },
-};
-
-export async function getGasPriceOptions(chain: ChainEntity, limitType: GasLimitType = 'other') {
+export async function getGasPriceOptions(chain: ChainEntity) {
   const api = await getGasPricer(chain);
   const result = await api.getGasPrice();
-  const gas = GAS_LIMIT_OVERRIDES[chain.id]?.[limitType];
-  return gas ? { ...result, gas } : result;
+  return result;
 }
